b8270510a50924ba753fb5241ecb0787d0e627b5
qi.b.zhang <qi.b.zhang@accenture.cn>
2025-12-11 16:45:26 +0800

    feat: update cards

--- blocks/centercards/centercards.js
+++ blocks/centercards/centercards.js
@@ -3,15 +3,14 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  console.log('block', block);
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'center-cards-card-image';
-      else div.className = 'center-cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
