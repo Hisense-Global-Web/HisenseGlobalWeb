bb0489350a65b7655b08f87a4ac2516ad0d2f97b
ruoqiong.xiao <ruoqiong.xiao@accenture.com>
2025-12-18 10:43:21 +0800

    feat: update style -new

--- blocks/icon-component/icon-component.js
+++ blocks/icon-component/icon-component.js
@@ -1,3 +1,67 @@
+let index = 0;
+const visibleCards = 4; // 视口内显示的卡片数量
+
+function getSlideWidth(block) {
+  const singleItem = block.querySelector('li');
+  // 获取实时宽度，解决 offsetWidth 为 0 的问题
+  const cardWidth = singleItem.offsetWidth;
+  const gap = 24;
+  return cardWidth + gap;
+}
+
+function updatePosition(block) {
+  const track = block.querySelector('.icon-track');
+  const items = block.querySelectorAll('li');
+  const moveDistance = index * getSlideWidth(block);
+  track.style.transform = `translateX(-${moveDistance}px)`;
+  block.querySelector('.slide-prev').disabled = (index === 0);
+  block.querySelector('.slide-next').disabled = (index >= items.length - visibleCards);
+}
+
+function bindEvent(block) {
+  const cards = block.querySelectorAll('.item');
+  block.querySelector('.slide-prev').addEventListener('click', () => {
+    if (index > 0) {
+      index -= 1;
+      updatePosition(block);
+    }
+  });
+  block.querySelector('.slide-next').addEventListener('click', () => {
+    if (index < cards.length - visibleCards) {
+      index += 1;
+      updatePosition(block);
+    }
+  });
+}
+
 export default async function decorate(block) {
-  block.classList.add('icon-block');
+  const iconContainer = document.createElement('div');
+  iconContainer.classList.add('icon-viewport');
+  const iconBlocks = document.createElement('ul');
+  iconBlocks.classList.add('icon-track');
+  iconContainer.appendChild(iconBlocks);
+
+  [...block.children].forEach((child) => {
+    const iconBlock = document.createElement('li');
+    child.classList.add('item');
+    [...child.children].forEach((item) => {
+      if (item.querySelector('picture')) {
+        item.querySelector('picture').closest('div').classList.add('item-picture');
+      }
+      if (!item.innerHTML) item.remove();
+    });
+    iconBlock.appendChild(child);
+    iconBlocks.appendChild(iconBlock);
+    // child.remove();
+  });
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('pagination');
+  buttonContainer.innerHTML = `
+      <button type="button" class="slide-prev" disabled>&lt;</button>
+      <button type="button" class="slide-next">&gt;</button>
+    `;
+  block.appendChild(buttonContainer);
+  block.appendChild(iconContainer);
+  bindEvent(block);
+  window.addEventListener('resize', updatePosition);
 }
