2c2914341604c3c30bdd99c43248eb6babb9d150
LiangYuan <v-liangyuan@mircosoft.com>
2025-12-27 18:23:15 +0800

    update header and footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,7 +8,22 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  let footerPath;
+  if (footerMeta) {
+    footerPath = new URL(footerMeta, window.location).pathname;
+  } else {
+    const currentPath = window.location.pathname;
+    const pathParts = currentPath.split('/').filter(Boolean);
+    if (currentPath.startsWith('/content/hisense')) {
+      // 使用前四级路径 + /footer
+      const basePath = `/${pathParts.slice(0, 4).join('/')}`;
+      footerPath = `${basePath}/footer`;
+    } else {
+      // 使用前二级路径 + /footer
+      const basePath = `/${pathParts.slice(0, 2).join('/')}`;
+      footerPath = `${basePath}/footer`;
+    }
+  }
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
