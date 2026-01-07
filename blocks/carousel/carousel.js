15d5a09ed4a742fd2ec9a20529be2bcdb073a0df
ruoqiong.xiao <ruoqiong.xiao@accenture.com>
2026-01-07 19:26:01 +0800

    feature banner mobile

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -150,7 +150,7 @@ function createSlide(block, row, slideIndex) {
       case 3:
         column.classList.add('carousel-item-button-theme');
         buttonTheme = column.querySelector('p')?.innerHTML || '';
-        if (buttonTheme) column.parentElement.lastElementChild.classList.add(buttonTheme);
+        if (buttonTheme) column.parentElement.querySelector('.button-container').classList.add(buttonTheme);
         column.innerHTML = '';
         break;
       default:
