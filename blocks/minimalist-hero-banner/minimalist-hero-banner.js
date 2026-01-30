cb196aebfbb1f5d3384ad83d1f37fb1e318de351
ruoqiong.xiao <ruoqiong.xiao@accenture.com>
2026-01-30 22:22:29 +0800

    finish pc

--- blocks/minimalist-hero-banner/minimalist-hero-banner.js
+++ blocks/minimalist-hero-banner/minimalist-hero-banner.js
@@ -1,12 +1,11 @@
-import { createElement } from "../../utils/dom-helper";
-
 export default async function decorate(block) {
-  const textContainer = createElement('div', 'text-container');
+  const textContainer = document.createElement('div');
   [...block.children].forEach(child => {
     if (child.querySelector('picture')) child.setAttribute('class','banner-image');
     else {
-      textContainer.append(...child.firstElementChild);
-      block.append(textContainer);
+      textContainer.append(...child.firstElementChild.children);
+      textContainer.setAttribute('class','text-container');
+      block.replaceChild(textContainer, child);
     }
   })
 }
