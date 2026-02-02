e17d03050d9f71478fe5e48c718b25ea783a9e50
JadeXiao <ruoqiong.xiao@accenture.cn>
2026-02-02 16:01:09 +0800

    Minimalist hero banner (#239)
    * update config
    
    * update config
    
    * finish pc
    
    * update mobile
    
    * update style
    
    * set nav-height
    
    * fix eslint
    
    ---------
    
    Co-authored-by: ruoqiong.xiao <ruoqiong.xiao@accenture.com>

---
+++ blocks/minimalist-hero-banner/minimalist-hero-banner.js
@@ -0,0 +1,16 @@
+export default async function decorate(block) {
+  const textContainer = document.createElement('div');
+  const isCompanyPage = window.location.pathname.includes('company-page');
+  if (isCompanyPage) {
+    const height = window.innerWidth > 860 ? '166px' : '112px';
+    document.documentElement.style.setProperty('--nav-height', height);
+  }
+  [...block.children].forEach((child) => {
+    if (child.querySelector('picture')) child.setAttribute('class', 'banner-image');
+    else {
+      textContainer.append(...child.firstElementChild.children);
+      textContainer.setAttribute('class', 'text-container h-grid-container');
+      block.replaceChild(textContainer, child);
+    }
+  });
+}
