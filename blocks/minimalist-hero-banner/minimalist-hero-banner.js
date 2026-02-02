34a2f710087d77bb56dc6ec998ff8eca62f5e237
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-02-02 15:58:53 +0800

    fix eslint

--- blocks/minimalist-hero-banner/minimalist-hero-banner.js
+++ blocks/minimalist-hero-banner/minimalist-hero-banner.js
@@ -2,15 +2,15 @@ export default async function decorate(block) {
   const textContainer = document.createElement('div');
   const isCompanyPage = window.location.pathname.includes('company-page');
   if (isCompanyPage) {
-    const height =  window.innerWidth > 860 ? '166px' : '112px';
+    const height = window.innerWidth > 860 ? '166px' : '112px';
     document.documentElement.style.setProperty('--nav-height', height);
   }
-  [...block.children].forEach(child => {
-    if (child.querySelector('picture')) child.setAttribute('class','banner-image');
+  [...block.children].forEach((child) => {
+    if (child.querySelector('picture')) child.setAttribute('class', 'banner-image');
     else {
       textContainer.append(...child.firstElementChild.children);
-      textContainer.setAttribute('class','text-container h-grid-container');
+      textContainer.setAttribute('class', 'text-container h-grid-container');
       block.replaceChild(textContainer, child);
     }
-  })
+  });
 }
