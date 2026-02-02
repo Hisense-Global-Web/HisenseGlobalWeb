f28208cdcc35f4ce197b1dfcad0623f06240f85c
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-02-02 15:53:51 +0800

    set nav-height

--- blocks/minimalist-hero-banner/minimalist-hero-banner.js
+++ blocks/minimalist-hero-banner/minimalist-hero-banner.js
@@ -1,5 +1,10 @@
 export default async function decorate(block) {
   const textContainer = document.createElement('div');
+  const isCompanyPage = window.location.pathname.includes('company-page');
+  if (isCompanyPage) {
+    const height =  window.innerWidth > 860 ? '166px' : '112px';
+    document.documentElement.style.setProperty('--nav-height', height);
+  }
   [...block.children].forEach(child => {
     if (child.querySelector('picture')) child.setAttribute('class','banner-image');
     else {
