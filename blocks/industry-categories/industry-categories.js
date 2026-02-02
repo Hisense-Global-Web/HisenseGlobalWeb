2419fd8399e34e284817c24c1a8329577e0a1b42
qi.b.zhang <qi.b.zhang@accenture.cn>
2026-02-02 14:05:54 +0800

    update

--- blocks/industry-categories/industry-categories.js
+++ blocks/industry-categories/industry-categories.js
@@ -4,23 +4,35 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.classList.add('card-item');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) { div.className = 'honors-awards-cards-card-image'; } else if (div.querySelector('.button-container')) div.className = 'honors-awards-cards-card-cta';
-      else div.className = 'honors-awards-cards-card-body';
-    });
-
-    ul.append(li);
+  const title = document.createElement('div');
+  [...block.children].forEach((row, i) => {
+    if (i <= 2) {
+      title.className = 'title';
+      title.append(row);
+    } else {
+      const li = document.createElement('li');
+      li.classList.add('card-item');
+      moveInstrumentation(row, li);
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'card-image';
+        } else {
+          div.className = 'card-body';
+          const tit = document.createElement('div');
+          const desc = document.createElement('div');
+          tit.append(div.firstElementChild);
+          desc.append(div.lastElementChild);
+          div.replaceChildren(tit, desc);
+        }
+      });
+      ul.append(li);
+    }
   });
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
-
-  block.replaceChildren(ul);
+  block.replaceChildren(title, ul);
 }
