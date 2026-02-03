b6e749f06d1f8ee2ddb07c0746e4a2974efcaa36
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-02-03 16:48:51 +0800

    update mobile style

--- blocks/gradient-text-image/gradient-text-image.js
+++ blocks/gradient-text-image/gradient-text-image.js
@@ -1,28 +1,17 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+
 export default async function decorate(block) {
-  // block.classList.add('h-grid-container');
+  const config = readBlockConfig(block);
   const textContainer = document.createElement('div');
-  [...block.children].forEach((child, index) => {
-    if (child.querySelector('picture')) child.setAttribute('class', 'gradient-image');
-    else {
-      switch (index) {
-        case 1:
-          child.firstElementChild?.classList.add('subtitle');
-          textContainer.append(child.firstElementChild);
-          break;
-        case 2:
-          child.firstElementChild?.classList.add('title');
-          textContainer.append(child.firstElementChild);
-          break;
-        case 3:
-          child.firstElementChild?.classList.add('text');
-          textContainer.append(child.firstElementChild);
-          break;
-        default:
-          break;
-      }
-      if (!child.textContent.trim()) child.remove();
+  [...block.children].forEach((child) => {
+    // avoid aem UE remove wrong
+    if (Object.keys(config).includes(child.firstElementChild.textContent)) {
+      child.setAttribute('class', child.firstElementChild.textContent);
+      child.firstElementChild.remove();
     }
-    textContainer.classList.add('text-container');
-    block.append(textContainer);
+    if (child.querySelector('picture')) child.setAttribute('class', 'gradient-image');
+    else textContainer.append(child);
   });
+  textContainer.classList.add('text-container');
+  block.append(textContainer);
 }
