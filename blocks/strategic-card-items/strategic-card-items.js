a1c54fc13fc1b1f1f95384a6f5e8f66fa7e08f78
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-02-04 16:42:23 +0800

    update config

--- blocks/strategic-card-items/strategic-card-items.js
+++ blocks/strategic-card-items/strategic-card-items.js
@@ -1,3 +1,13 @@
+import {readBlockConfig} from '../../scripts/aem.js';
+import { isUniversalEditor } from '../../utils/ue-helper.js';
 export default function decorate(block) {
-    block.id = 'strategic-card-items';
+    const config = readBlockConfig(block);
+    if (config.columns && isUniversalEditor()) {
+        const addButton = document.querySelectorAll('button[aria-label="添加"]');
+        if (config.columns > 1) {
+            for (let index = 0; index < config.columns; index++) {
+                addButton.click();
+            }
+        }
+    }
 }
