065baa539ce1e0e17076170400faa57962cca74b
Lv <shanshan.lv@accenture.cn>
2026-02-04 16:48:48 +0800

    feat: card-grid.js lint issue

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -3,12 +3,12 @@ export default async function decorate(block) {
   featureItemsWrapperEl.className = 'feature-items-wrapper';
   [...block.children].forEach((child) => {
     child.className = 'feature-item-box';
-      child.firstElementChild.className = 'item-img-box';
-      const secondChild = child.children[1];
-      secondChild.className = 'item-text-box';
-      secondChild.firstElementChild.className = 'item-title';
-      secondChild.querySelector('p:nth-child(2)').className = 'item-text';
-      featureItemsWrapperEl.append(child);
+    child.firstElementChild.className = 'item-img-box';
+    const secondChild = child.children[1];
+    secondChild.className = 'item-text-box';
+    secondChild.firstElementChild.className = 'item-title';
+    secondChild.querySelector('p:nth-child(2)').className = 'item-text';
+    featureItemsWrapperEl.append(child);
   });
   block.append(featureItemsWrapperEl);
 }
