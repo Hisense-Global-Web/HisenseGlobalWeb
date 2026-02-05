db23c6f8a3aebfdb721a4d3c8b765ab6d7d47ce7
avalonXH <hang.xiao@accenture.com>
2026-02-05 14:49:16 +0800

    title

--- blocks/article-title/article-title.js
+++ blocks/article-title/article-title.js
@@ -1,7 +1,8 @@
 import { readBlockConfig } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-  console.log(1111);
-  const config = readBlockConfig(block);
-  console.log(block, config);
+  [...block.children].forEach((child) => {
+    child.setAttribute('class', child.firstElementChild.textContent);
+    child.firstElementChild.remove();
+  });
 }
