86379f4d94b98eb777cf80cb13cebb6e7547d8ff
Lv <shanshan.lv@accenture.cn>
2026-02-05 16:31:35 +0800

    feat: update card grid json and style

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -1,18 +1,9 @@
 export default async function decorate(block) {
   const featureItemsWrapperEl = document.createElement('div');
   featureItemsWrapperEl.className = 'feature-items-wrapper';
-  // const itemTextBoxEl = document.createElement('div');
-  // itemTextBoxEl.className = 'item-text-box';
+
   [...block.children].forEach((child) => {
     child.className = 'feature-item-box';
-    // child.firstElementChild.className = 'item-img-box';
-
-    // const secondChild = child.children[1];
-    // secondChild.className = 'item-text-box';
-    // secondChild.firstElementChild.className = 'item-subtitle';
-    // secondChild.querySelector('p:nth-child(2)').className = 'item-title';
-    // secondChild.querySelector('p:nth-child(3)').className = 'item-text';
-    // featureItemsWrapperEl.append(child);
     const itemTextBoxEl = document.createElement('div');
     itemTextBoxEl.className = 'item-text-box';
     [...child.children].forEach((item, itemIndex) => {
