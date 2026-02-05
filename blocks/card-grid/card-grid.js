93d778030268355a4bb44e2e638cf4a064031af2
Lv <shanshan.lv@accenture.cn>
2026-02-05 16:18:22 +0800

    feat: update card grid js

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -1,28 +1,33 @@
 export default async function decorate(block) {
   const featureItemsWrapperEl = document.createElement('div');
   featureItemsWrapperEl.className = 'feature-items-wrapper';
-  const itemTextBoxEl = document.createElement('div');
-  itemTextBoxEl.className = 'item-text-box';
+  // const itemTextBoxEl = document.createElement('div');
+  // itemTextBoxEl.className = 'item-text-box';
   [...block.children].forEach((child) => {
     child.className = 'feature-item-box';
-    child.firstElementChild.className = 'item-img-box';
+    // child.firstElementChild.className = 'item-img-box';
+
     // const secondChild = child.children[1];
     // secondChild.className = 'item-text-box';
     // secondChild.firstElementChild.className = 'item-subtitle';
     // secondChild.querySelector('p:nth-child(2)').className = 'item-title';
     // secondChild.querySelector('p:nth-child(3)').className = 'item-text';
     // featureItemsWrapperEl.append(child);
+    const itemTextBoxEl = document.createElement('div');
+    itemTextBoxEl.className = 'item-text-box';
     [...child.children].forEach((item, itemIndex) => {
-      // console.log(item, 'iiiii')
       if (itemIndex > 0) {
-        // console.log(item, 'iiii');
         const type = item.firstElementChild?.textContent;
-        // console.log(type, 'ttt');
         item.classList.add(type);
+        item.firstElementChild.remove();
         itemTextBoxEl.append(item);
+      } else {
+        item.className = 'item-img-box';
       }
     });
+
     child.append(itemTextBoxEl);
+    featureItemsWrapperEl.append(child);
   });
-  // block.append(itemTextBoxWrapperEl);
+  block.append(featureItemsWrapperEl);
 }
