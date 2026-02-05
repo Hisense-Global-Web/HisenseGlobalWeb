fb29acae3a9f664ef3645b9b222e80a4c3810aef
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-02-05 15:20:48 +0800

    update button variant

--- blocks/cross-page-redirect/cross-page-redirect.js
+++ blocks/cross-page-redirect/cross-page-redirect.js
@@ -1,7 +1,12 @@
 export default function decorate(block) {
-    const [ crossWrapper ]  = block;
-    crossWrapper.children.forEach(child => {
-        console.log(child);
-        
-    });
+    const [ headline, bodyCopy, button ] = block.children;
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('textContainer');
+    textContainer.append(headline,bodyCopy);
+    headline.classList.add('headline');
+    bodyCopy.classList.add('bodyCopy');
+    button.classList.add('button-container');
+    block.prepend(textContainer);
+    button.querySelector('a').textContent = button.firstElementChild.lastElementChild.textContent;
+    button.firstElementChild.lastElementChild.remove();
 }
