bad7df5858a2c337c567f04dfed3faa716c006ec
JadeXiao <ruoqiong.xiao@accenture.cn>
2026-02-05 20:13:42 +0800

    Cross page redirect (#257)
    * update config
    
    * update config
    
    * update css
    
    * update cta's name
    
    * update button variant
    
    * finish mobile style
    
    * fix: eslint
    
    * fix gap value
    
    * update description
    
    * update json

---
+++ blocks/cross-page-redirect/cross-page-redirect.js
@@ -0,0 +1,14 @@
+export default function decorate(block) {
+  const [headline, bodyCopy, button] = block.children;
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('text-container');
+  textContainer.append(headline, bodyCopy);
+  headline.classList.add('headline');
+  bodyCopy.classList.add('bodyCopy');
+  button.classList.add('button-container');
+  block.prepend(textContainer);
+  button.querySelector('a').textContent = button.firstElementChild.lastElementChild.textContent;
+  button.querySelector('a').classList.add(`bg-${button.firstElementChild.firstElementChild.textContent}`);
+  button.firstElementChild.firstElementChild.remove();
+  button.firstElementChild.lastElementChild.remove();
+}
