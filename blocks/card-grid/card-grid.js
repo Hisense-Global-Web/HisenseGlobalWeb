44908f92ca2aca079362aa70fe87176df71452da
lvss-33 <shanshan.lv@accenture.cn>
2026-02-05 17:17:25 +0800

    card grid add subtitle and update section background position style  (#256)

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -1,13 +1,23 @@
 export default async function decorate(block) {
   const featureItemsWrapperEl = document.createElement('div');
   featureItemsWrapperEl.className = 'feature-items-wrapper';
+
   [...block.children].forEach((child) => {
     child.className = 'feature-item-box';
-    child.firstElementChild.className = 'item-img-box';
-    const secondChild = child.children[1];
-    secondChild.className = 'item-text-box';
-    secondChild.firstElementChild.className = 'item-title';
-    secondChild.querySelector('p:nth-child(2)').className = 'item-text';
+    const itemTextBoxEl = document.createElement('div');
+    itemTextBoxEl.className = 'item-text-box';
+    [...child.children].forEach((item, itemIndex) => {
+      if (itemIndex > 0) {
+        const type = item.firstElementChild?.textContent;
+        item.classList.add(type);
+        item.firstElementChild.remove();
+        itemTextBoxEl.append(item);
+      } else {
+        item.className = 'item-img-box';
+      }
+    });
+
+    child.append(itemTextBoxEl);
     featureItemsWrapperEl.append(child);
   });
   block.append(featureItemsWrapperEl);
