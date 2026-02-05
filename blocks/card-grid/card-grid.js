f9c348e1dbf4bdeb986d55d869279130b57e7fbb
Lv <shanshan.lv@accenture.cn>
2026-02-05 15:59:08 +0800

    feat: update card grid js

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -1,15 +1,28 @@
 export default async function decorate(block) {
   const featureItemsWrapperEl = document.createElement('div');
   featureItemsWrapperEl.className = 'feature-items-wrapper';
-  // [...block.children].forEach((child) => {
-  //   child.className = 'feature-item-box';
-  //   child.firstElementChild.className = 'item-img-box';
-  //   const secondChild = child.children[1];
-  //   secondChild.className = 'item-text-box';
-  //   secondChild.firstElementChild.className = 'item-subtitle';
-  //   secondChild.querySelector('p:nth-child(2)').className = 'item-title';
-  //   secondChild.querySelector('p:nth-child(3)').className = 'item-text';
-  //   featureItemsWrapperEl.append(child);
-  // });
-  // block.append(featureItemsWrapperEl);
+  const itemTextBoxEl = document.createElement('div');
+  itemTextBoxEl.className = 'item-text-box';
+  [...block.children].forEach((child) => {
+    child.className = 'feature-item-box';
+    child.firstElementChild.className = 'item-img-box';
+    // const secondChild = child.children[1];
+    // secondChild.className = 'item-text-box';
+    // secondChild.firstElementChild.className = 'item-subtitle';
+    // secondChild.querySelector('p:nth-child(2)').className = 'item-title';
+    // secondChild.querySelector('p:nth-child(3)').className = 'item-text';
+    // featureItemsWrapperEl.append(child);
+    [...child.children].forEach((item, itemIndex) => {
+      // console.log(item, 'iiiii')
+      if (itemIndex > 0) {
+        // console.log(item, 'iiii');
+        const type = item.firstElementChild?.textContent;
+        // console.log(type, 'ttt');
+        item.classList.add(type);
+        itemTextBoxEl.append(item);
+      }
+    });
+    child.append(itemTextBoxEl);
+  });
+  // block.append(itemTextBoxWrapperEl);
 }
