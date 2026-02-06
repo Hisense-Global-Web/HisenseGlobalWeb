c61e47abaa3db773c963b1c8fedf352fb314fac6
alexxxxxsun <shuangrui.a.sun@accenture.cn>
2026-02-06 17:49:50 +0800

    feat: update number animation

--- blocks/global-presence/global-presence.js
+++ blocks/global-presence/global-presence.js
@@ -1,6 +1,7 @@
 import { createElement, debounce } from '../../utils/dom-helper.js';
 import { loadScrollTrigger } from '../../utils/animation-helper.js';
 import { isUniversalEditorAsync } from '../../utils/ue-helper.js';
+import { loadScript } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   // ========== CONSTRUCT DOM [START] ========== //
@@ -46,6 +47,29 @@ export default async function decorate(block) {
     if (item.children.length === 2) {
       const statsItem = createElement('li', 'global-presence-stats-list-item');
       statsItem.innerHTML = item.innerHTML;
+      const firstItem = statsItem.querySelector('p');
+      const animatedItem = createElement('div', 'animated-stats-item');
+      const itemContent = firstItem.textContent;
+      itemContent.split('')
+        .forEach((char) => {
+          const wrapper = createElement('div', 'number-wrapper');
+          const isNumber = Number(char)
+            .toString() !== 'NaN';
+          const numberValue = Number(char);
+          const finalNumberEle = createElement('div', isNumber ? 'number-char final-number-char' : '');
+          finalNumberEle.textContent = char;
+          if (isNumber && numberValue >= 0) {
+            // eslint-disable-next-line no-plusplus
+            for (let i = 0; i < numberValue; i++) {
+              const div = createElement('div', 'number-char');
+              div.textContent = i.toString();
+              wrapper.appendChild(div);
+            }
+          }
+          wrapper.appendChild(finalNumberEle);
+          animatedItem.appendChild(wrapper);
+        });
+      firstItem.replaceWith(animatedItem);
       statsList.appendChild(statsItem);
       item.remove();
     }
@@ -72,14 +96,24 @@ export default async function decorate(block) {
     return;
   }
 
+  if (!window.SplitText) {
+    try {
+      await loadScript('https://cdn.jsdelivr.net/npm/gsap@3.14.1/dist/SplitText.min.js');
+    } catch (error) {
+      return;
+    }
+  }
+
   const {
     gsap,
     ScrollTrigger,
+    SplitText,
   } = window;
 
   ScrollTrigger.config({ autoRefreshEvents: 'DOMContentLoaded,load' });
 
   gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(SplitText);
 
   let scrollTriggerInstance = null;
 
@@ -92,12 +126,25 @@ export default async function decorate(block) {
 
   const animate = () => {
     cleanup();
+
+    const items = gsap.utils.toArray('.animated-stats-item');
+    items.forEach((item) => {
+      const targets = item.querySelector('.number-wrapper');
+      targets.forEach((target) => {
+        gsap.to(target, {
+          yPercent: -100,
+          duration: 0.2,
+          yoyo: true,
+        }, '+=0.1');
+      });
+    });
   };
 
-  block.addEventListener('load', animate, { once: true });
+  animate();
+  // block.addEventListener('load', animate, { once: true });
 
   const handleResize = debounce(() => {
-    // animate();
+    animate();
     // Refresh ScrollTrigger after a brief delay to ensure DOM has updated
     setTimeout(() => {
       ScrollTrigger.refresh();
