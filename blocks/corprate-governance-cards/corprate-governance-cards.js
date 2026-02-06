927984ddf50c0b9183e3fd00177bd1a326d5dc8a
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-02-06 18:01:48 +0800

    update

--- blocks/corprate-governance-cards/corprate-governance-cards.js
+++ blocks/corprate-governance-cards/corprate-governance-cards.js
@@ -2,13 +2,13 @@ export default function decorate(block) {
   block.id = 'coprate-governance-cards';
   [...block.children].forEach(cardItem => {
     const [iconDiv, title, text, linkDiv] = cardItem.children;
-    iconDiv.classList.add('icon-div');
+    iconDiv?.classList.add('icon-div');
     const textContent = document.createElement('div');
     textContent.classList.add('text-content');
     textContent.append(title, text);
-    title.classList.add('title');
-    text.classList.add('text');
-    linkDiv.querySelectorAll('a').forEach(button => {
+    title?.classList.add('title');
+    text?.classList.add('text');
+    linkDiv?.querySelectorAll('a').forEach(button => {
       button.closest('div').classList.add('link-div');
       button.textContent = button.nextElementSibling?.textContent || button.parentElement.nextElementSibling.textContent;
       button.nextElementSibling?.remove();
