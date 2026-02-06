edd835c607ddf1d1bfc1e3cd8efb142dfce02dd9
guoli.zhang <guoli.zhang@accenture.cn>
2026-02-06 18:39:02 +0800

    feat: change AddionalSupportCard.

--- blocks/addional-support-card/addional-support-card.js
+++ blocks/addional-support-card/addional-support-card.js
@@ -2,18 +2,24 @@ export default function decorate(block) {
   try {
     const elementItems = [...block.children];
     elementItems.forEach((element) => {
-      element.classList.add('category-support-card-item');
-      const [icon, description, link] = element.children;
-      icon?.classList?.add('category-support-card-item-icon');
-      description?.classList?.add('category-support-card-item-description');
-      const linkUrl = link.querySelector('a') ? link.querySelector('a').href : '#';
-      link?.remove();
-      element.addEventListener('click', () => {
-        window.location.href = linkUrl;
-      });
+      element.classList.add('addional-support-card-item');
+      const [icon, title, subtitle, buttonLink, buttonText] = element.children;
+      icon?.classList?.add('addional-support-card-item-icon');
+      title?.classList?.add('addional-support-card-item-title');
+      subtitle?.classList?.add('addional-support-card-item-subtitle');
+      buttonLink?.classList?.add('addional-support-card-item-link');
+      buttonText?.classList?.add('addional-support-card-item-link-text');
+      // buttonLink?.querySelector('a')?.classList?.add('addional-support-card-item-link-a');
+      buttonLink.querySelector('a').innerHTML = buttonText?.querySelector('p')?.innerHTML || '';
+      buttonText?.remove();
+      // const linkUrl = link.querySelector('a') ? link.querySelector('a').href : '#';
+      // link?.remove();
+      // element.addEventListener('click', () => {
+      //   window.location.href = linkUrl;
+      // });
     });
   } catch (error) {
     /* eslint-disable-next-line no-console */
-    console.error('Category Support Card block decoration error:', error);
+    console.error('Addional Support Card block decoration error:', error);
   }
 }
