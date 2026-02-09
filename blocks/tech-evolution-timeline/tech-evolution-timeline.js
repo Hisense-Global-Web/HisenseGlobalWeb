41b5f3d203a40817d3fb4e4c058aaaef02c5fad9
Lv <shanshan.lv@accenture.cn>
2026-02-09 18:16:38 +0800

    feat: tech evolution timeline json js and style update

--- blocks/tech-evolution-timeline/tech-evolution-timeline.js
+++ blocks/tech-evolution-timeline/tech-evolution-timeline.js
@@ -1,19 +1,45 @@
 export default async function decorate(block) {
-  console.log(block, 'bbbb');
   const timelineWrapper = document.createElement('div');
   timelineWrapper.className = 'timeline-wrapper';
   const techCardWrapper = document.createElement('div');
   techCardWrapper.className = 'tech-card-wrapper';
-  [...block.children].forEach(row => {
+  const timeGreenLineEL = document.createElement('div');
+  timeGreenLineEL.classList = 'time-green-line';
+  [...block.children].forEach((row) => {
     // component type name
     const type = row.firstElementChild?.textContent?.trim() || '';
     row.className = type;
     row.firstElementChild.remove();
-    [...row.children].forEach((column) => {
-      console.log(column, 'cccc')
+
+    const techCardInfoEl = document.createElement('div');
+    techCardInfoEl.className = 'tech-card-info';
+    [...row.children].forEach((column, colIndex) => {
       const columnType = column.firstElementChild?.textContent?.trim() || '';
       column.className = columnType;
-      // column.firstElementChild.remove();
-    })
+      column.firstElementChild.remove();
+      // tech card info
+      if (type === 'component-tech-card') {
+        if (colIndex > 0) {
+          techCardInfoEl.append(column);
+        }
+      }
+    });
+
+    // type: component-timeline and component-tech-card
+    if (type === 'component-timeline') {
+      const timelineDotEl = document.createElement('div');
+      timelineDotEl.className = 'timeline-dot';
+      const circleImg = document.createElement('img');
+      circleImg.src = '/content/dam/hisense/us/common-icons/timeline-circle.svg';
+      circleImg.alt = 'Timeline dot';
+      timelineDotEl.append(circleImg);
+      row.prepend(timelineDotEl);
+      timelineWrapper.append(row);
+    } else {
+      row.append(techCardInfoEl);
+      techCardWrapper.append(row);
+      techCardWrapper.append(row);
+    }
   });
+  block.append(timeGreenLineEL, timelineWrapper, techCardWrapper);
 }
