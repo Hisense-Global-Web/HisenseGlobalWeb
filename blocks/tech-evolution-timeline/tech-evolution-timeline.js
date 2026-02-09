9e713c8ed461d03b82fa8624f6700e6d1cc52c1c
Lv <shanshan.lv@accenture.cn>
2026-02-09 15:27:57 +0800

    feat: update timeline json background add type

--- blocks/tech-evolution-timeline/tech-evolution-timeline.js
+++ blocks/tech-evolution-timeline/tech-evolution-timeline.js
@@ -1,3 +1,19 @@
 export default async function decorate(block) {
-  console.log(block, 'bbbb')
+  console.log(block, 'bbbb');
+  const timelineWrapper = document.createElement('div');
+  timelineWrapper.className = 'timeline-wrapper';
+  const techCardWrapper = document.createElement('div');
+  techCardWrapper.className = 'tech-card-wrapper';
+  [...block.children].forEach(row => {
+    // component type name
+    const type = row.firstElementChild?.textContent?.trim() || '';
+    row.className = type;
+    row.firstElementChild.remove();
+    [...row.children].forEach((column) => {
+      console.log(column, 'cccc')
+      const columnType = column.firstElementChild?.textContent?.trim() || '';
+      column.className = columnType;
+      // column.firstElementChild.remove();
+    })
+  });
 }
