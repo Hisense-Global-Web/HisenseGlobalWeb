95b53b57015f5a127be7f70d564a6ab125c9fb27
guoli.zhang <guoli.zhang@accenture.cn>
2026-02-09 17:23:19 +0800

    feat: change css and js.

--- blocks/addional-support-card/addional-support-card.js
+++ blocks/addional-support-card/addional-support-card.js
@@ -3,20 +3,32 @@ export default function decorate(block) {
     const elementItems = [...block.children];
     elementItems.forEach((element) => {
       element.classList.add('addional-support-card-item');
-      const [icon, title, subtitle, buttonLink, buttonText] = element.children;
+      const [icon, title, subtitle, workingTime, responseTime, buttonLink, buttonText] = element.children;
       icon?.classList?.add('addional-support-card-item-icon');
       title?.classList?.add('addional-support-card-item-title');
       subtitle?.classList?.add('addional-support-card-item-subtitle');
-      buttonLink?.classList?.add('addional-support-card-item-link');
+      // 将working time和response time合并到一个div中
+      workingTime?.classList?.add('working-time');
+      responseTime?.classList?.add('response-time');
+      const timeDivContainer = document.createElement('div');
+      timeDivContainer.appendChild(workingTime);
+      timeDivContainer.appendChild(responseTime);
+      element.insertBefore(timeDivContainer, buttonLink);
+      timeDivContainer.classList.add('addional-support-card-item-time-container');
       buttonText?.classList?.add('addional-support-card-item-link-text');
-      // buttonLink?.querySelector('a')?.classList?.add('addional-support-card-item-link-a');
+      const buttonALink = buttonLink.querySelector('a');
+      buttonALink.classList.remove('button');
+      buttonALink.classList.add('addional-support-card-item-link');
+      // Live Chart 可能需要第三方来实现，所以此处暂时注释掉
+      // if (index === 0) {
+      //   const circleDiv = document.createElement('div');
+      //   circleDiv.classList.add('main-circle');
+      //   timeDivContainer.insertBefore(circleDiv, workingTime);
+      //   workingTime?.classList?.add('main-working-time');
+      //   responseTime?.classList?.add('main-response-time');
+      // }
       buttonLink.querySelector('a').innerHTML = buttonText?.querySelector('p')?.innerHTML || '';
       buttonText?.remove();
-      // const linkUrl = link.querySelector('a') ? link.querySelector('a').href : '#';
-      // link?.remove();
-      // element.addEventListener('click', () => {
-      //   window.location.href = linkUrl;
-      // });
     });
   } catch (error) {
     /* eslint-disable-next-line no-console */
