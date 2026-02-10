5b0eae3ff0cb7b1c34a5ccfb9e866b7ad5011b6e
Lv <shanshan.lv@accenture.cn>
2026-02-10 15:50:02 +0800

    feat: update icon url

--- blocks/tech-evolution-timeline/tech-evolution-timeline.js
+++ blocks/tech-evolution-timeline/tech-evolution-timeline.js
@@ -30,7 +30,7 @@ export default async function decorate(block) {
       const timelineDotEl = document.createElement('div');
       timelineDotEl.className = 'timeline-dot';
       const circleImg = document.createElement('img');
-      circleImg.src = '/content/dam/hisense/us/common-icons/timeline-circle.svg';
+      circleImg.src = '/content/dam/hisense/us/common-icons/custom-icons/timeline-circle.svg';
       circleImg.alt = 'Timeline dot';
       timelineDotEl.append(circleImg);
       row.prepend(timelineDotEl);
