50b368edc7ccb1850f92e7419dfb5beae1dcb6c9
Lv <shanshan.lv@accenture.cn>
2026-02-11 09:47:42 +0800

    feat: del show field type

--- blocks/enterprise-tech/enterprise-tech.js
+++ blocks/enterprise-tech/enterprise-tech.js
@@ -1,18 +1,18 @@
-export default async function decorate(block) {
-  // console.log(block, 'bbb');
-  const isEditMode = block.hasAttribute('data-aue-resource');
-  if (isEditMode) {
-    return;
-  }
-  [...block.children].forEach((row) => {
-    // component type name
-    const type = row.firstElementChild?.textContent?.trim() || '';
-    row.className = type;
-    row.firstElementChild.remove();
-    // [...row.children].forEach((column, colIndex) => {
-    //   const columnType = column.firstElementChild?.textContent?.trim() || '';
-    //   column.className = columnType;
-    //   column.firstElementChild.remove();
-    // });
-  });
-}
+// export default async function decorate(block) {
+//   // console.log(block, 'bbb');
+//   const isEditMode = block.hasAttribute('data-aue-resource');
+//   if (isEditMode) {
+//     return;
+//   }
+//   [...block.children].forEach((row) => {
+//     // component type name
+//     const type = row.firstElementChild?.textContent?.trim() || '';
+//     row.className = type;
+//     row.firstElementChild.remove();
+//     // [...row.children].forEach((column, colIndex) => {
+//     //   const columnType = column.firstElementChild?.textContent?.trim() || '';
+//     //   column.className = columnType;
+//     //   column.firstElementChild.remove();
+//     // });
+//   });
+// }
