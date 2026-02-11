a558c311487aaef0e2ff580d4a814b698e2d4a54
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-02-11 17:37:52 +0800

    fix eslint

--- blocks/sustainability-dashboard-list/sustainability-dashboard-list.js
+++ blocks/sustainability-dashboard-list/sustainability-dashboard-list.js
@@ -1,9 +1,9 @@
 export default function decorate(block) {
-    const config = ['title', 'value', 'description'];
-    [...block.children].forEach((child) => {
-        child.classList.add('sustainability-dashboard-list-item');
-        [...child.children].forEach((grandChild, index) => {
-            grandChild.classList.add(config[index]);
-        })
-    })
+  const config = ['title', 'value', 'description'];
+  [...block.children].forEach((child) => {
+    child.classList.add('sustainability-dashboard-list-item');
+    [...child.children].forEach((grandChild, index) => {
+      grandChild.classList.add(config[index]);
+    });
+  });
 }
