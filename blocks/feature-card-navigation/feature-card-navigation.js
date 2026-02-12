9ff78999d6e0c8c9969143a8150dcf7bbe887a52
lvss-33 <shanshan.lv@accenture.cn>
2026-02-12 15:32:16 +0800

    feat: add feature card navigation block (#310)

---
+++ blocks/feature-card-navigation/feature-card-navigation.js
@@ -0,0 +1,23 @@
+export default async function decorate(block) {
+  const cardNavigationWrapperEl = document.createElement('div');
+  cardNavigationWrapperEl.className = 'card-navigation-wrapper';
+
+  [...block.children].forEach((row) => {
+    row.className = 'card-box';
+    cardNavigationWrapperEl.append(row);
+    [...row.children].forEach((column, colIndex) => {
+      switch (colIndex) {
+        case 0:
+          column.className = 'card-image-box';
+          break;
+        case 1:
+          column.className = 'card-title';
+          break;
+        default:
+          column.className = 'card-description';
+          break;
+      }
+    });
+  });
+  block.append(cardNavigationWrapperEl);
+}
