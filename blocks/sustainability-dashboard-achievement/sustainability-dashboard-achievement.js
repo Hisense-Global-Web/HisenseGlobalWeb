40ce1780871fed647c5645dbbf6673d7e2f9870f
ruoqiong.xiao <ruoqiong.xiao@accenture.com>
2026-02-13 20:42:04 +0800

    update dom and style

--- blocks/sustainability-dashboard-achievement/sustainability-dashboard-achievement.js
+++ blocks/sustainability-dashboard-achievement/sustainability-dashboard-achievement.js
@@ -2,22 +2,64 @@ import { readBlockConfig } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const config = readBlockConfig(block);
-  console.log(config, block);
+  const textContainer = document.createElement('div');
+  textContainer.className = 'text-content';
+  const textArea = document.createElement('div');
+  textArea.className = 'text-area';
+  const btnDiv = document.createElement('div');
+  btnDiv.className = 'btn-cta';
+  const titleArea = document.createElement('div');
+  titleArea.className = 'title-area';
+  textArea.appendChild(titleArea);
+  const items = document.createElement('div');
+  items.className = 'matrix-block-items';
   [...block.children].forEach((child) => {
-    console.log(child);
-    if (child.textContent.includes('matrix')) {
-      block.append(...child.children);
-    }
     if (Object.keys(config).includes(child.firstElementChild?.textContent.trim().toLowerCase())) {
       child.classList.add(child.firstElementChild?.textContent.trim());
       child.firstElementChild.remove();
     }
+    if(child.classList.contains('matrix-block-item')) {
+      const label = ['matrix-label','matrix-value'];
+      console.log(child);
+      // blockItem 最少两个节点，最多4个节点
+      const blockItem = Array.from(child.children);
+      console.log(blockItem,'tem');
+      for (let i = 0; i < blockItem.length; i += 2) {
+        const labelDiv = blockItem[i];
+        const valueDiv = blockItem[i + 1];
+  
+        // 获取标签内容，比如 "matrix-label" 或 "matrix-value"
+        const labelText = labelDiv?.querySelector('p')?.textContent.trim();
+  
+        if (labelText && valueDiv) {
+          // 将内容作为类名添加
+          const className = labelText.toLowerCase();
+          valueDiv.classList.add(className);
+          labelDiv.remove();
+        }
+      }
+    }
+    if(!child.classList.contains('image')) {
+      if(!child.className.includes('btn')) {
+        if(child.className !== 'matrix-block-item') {
+          titleArea.appendChild(child);
+        } else items.appendChild(child);
+      } else if(child.className.includes('btn')) {
+        btnDiv.appendChild(child);
+      } else {
+        textContainer.appendChild(child);
+      }
+    }
   });
-  // const config = ['title', 'value', 'description'];
-  // [...block.children].forEach((child) => {
-  //   child.classList.add('sustainability-dashboard-list-item');
-  //   [...child.children].forEach((grandChild, index) => {
-  //     grandChild.classList.add(config[index]);
-  //   });
-  // });
+  [...items.children].forEach(item=> {
+    if(item !== [...items.children][items.children.length-1]) {
+      const line = document.createElement('span');
+      line.className = 'line';
+      item.after(line);
+    }
+  })
+  textArea.appendChild(items);
+  textContainer.appendChild(textArea);
+  textContainer.appendChild(btnDiv);
+  block.appendChild(textContainer);
 }
