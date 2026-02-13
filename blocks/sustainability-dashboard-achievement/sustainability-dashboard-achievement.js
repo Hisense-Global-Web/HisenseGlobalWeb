c66461471589c9a5e89f63ff9c5553a56bb3520e
ruoqiong.xiao <ruoqiong.xiao@accenture.com>
2026-02-13 12:05:59 +0800

    update dashboard

---
+++ blocks/sustainability-dashboard-achievement/sustainability-dashboard-achievement.js
@@ -0,0 +1,11 @@
+export default function decorate(block) {
+  console.log(block);
+  
+  // const config = ['title', 'value', 'description'];
+  // [...block.children].forEach((child) => {
+  //   child.classList.add('sustainability-dashboard-list-item');
+  //   [...child.children].forEach((grandChild, index) => {
+  //     grandChild.classList.add(config[index]);
+  //   });
+  // });
+}
