9ad827170ec6860f04ef20b6708ec6c99a1bcb8a
ruoqiong.xiao <ruoqiong.xiao@accenture.com>
2026-02-13 16:22:45 +0800

    zancun

--- blocks/sustainability-dashboard-achievement/sustainability-dashboard-achievement.js
+++ blocks/sustainability-dashboard-achievement/sustainability-dashboard-achievement.js
@@ -2,8 +2,17 @@ import { readBlockConfig } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const config = readBlockConfig(block);
-  console.log(config,block);
-  
+  console.log(config, block);
+  [...block.children].forEach((child) => {
+    console.log(child);
+    if (child.textContent.includes('matrix')) {
+      block.append(...child.children);
+    }
+    if (Object.keys(config).includes(child.firstElementChild?.textContent.trim().toLowerCase())) {
+      child.classList.add(child.firstElementChild?.textContent.trim());
+      child.firstElementChild.remove();
+    }
+  });
   // const config = ['title', 'value', 'description'];
   // [...block.children].forEach((child) => {
   //   child.classList.add('sustainability-dashboard-list-item');
