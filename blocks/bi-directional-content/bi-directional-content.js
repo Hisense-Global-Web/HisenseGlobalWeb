8f9b4c72089fb98fd53f826f7de7a63da9089a98
qi.b.zhang <qi.b.zhang@accenture.cn>
2026-02-14 15:56:38 +0800

    update

---
+++ blocks/bi-directional-content/bi-directional-content.js
@@ -0,0 +1,29 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('div');
+  ul.classList.add('card-list');
+  [...block.children].forEach((row) => {
+    if (row.children.length > 1) {
+      const li = document.createElement('div');
+      li.classList.add('card-item');
+      moveInstrumentation(row, li);
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.querySelector('picture')) div.className = 'card-image';
+        else {
+          div.className = 'card-body';
+        }
+      });
+      ul.append(li);
+    }
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.replaceChildren(ul);
+}
