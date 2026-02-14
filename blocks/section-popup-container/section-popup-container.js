91c17af11d3eb1c2ecd7bd0ce99edd4e488a7043
Lv <shanshan.lv@accenture.cn>
2026-02-14 18:05:16 +0800

    feat: update popup container js

--- blocks/section-popup-container/section-popup-container.js
+++ blocks/section-popup-container/section-popup-container.js
@@ -18,6 +18,7 @@ export default function decorate(block) {
         } else {
           column.parentElement.closest('.section-popup-container-container').classList.remove('popup-show');
         }
+        column.firstElementChild.remove();
       }
     });
   });
