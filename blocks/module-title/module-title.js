260e9672fe6de3007ef8a717ede3354c3974df06
Lv <shanshan.lv@accenture.cn>
2026-02-26 15:48:48 +0800

    feat: test module title show tow 2

--- blocks/module-title/module-title.js
+++ blocks/module-title/module-title.js
@@ -1,20 +1,20 @@
-export default async function decorate(block) {
-  [...block.children].forEach((child) => {
-    child.setAttribute('class', child.firstElementChild.textContent);
-    child.firstElementChild.remove();
-  });
+// export default async function decorate(block) {
+//   // [...block.children].forEach((child) => {
+//   //   child.setAttribute('class', child.firstElementChild.textContent);
+//   //   child.firstElementChild.remove();
+//   // });
 
-  // module title text align type
+//   // module title text align type
 
-  // const textAlignTypeEl = document.querySelector('.text-align-type p');
+//   // const textAlignTypeEl = document.querySelector('.text-align-type p');
 
-  // if (textAlignTypeEl) {
-  //   const textAlignType = document.querySelector('.text-align-type p').innerHTML;
-  //   document.querySelector('.text-align-type').parentElement.classList.add(textAlignType);
-  //   document.querySelector('.text-align-type').remove();
-  //   // if (textAlignType) {
-  //   //   document.querySelector('.text-align-type').parentElement.classList.add(textAlignType);
-  //   //   document.querySelector('.text-align-type').remove();
-  //   // }
-  // }
-}
+//   // if (textAlignTypeEl) {
+//   //   const textAlignType = document.querySelector('.text-align-type p').innerHTML;
+//   //   document.querySelector('.text-align-type').parentElement.classList.add(textAlignType);
+//   //   document.querySelector('.text-align-type').remove();
+//   //   // if (textAlignType) {
+//   //   //   document.querySelector('.text-align-type').parentElement.classList.add(textAlignType);
+//   //   //   document.querySelector('.text-align-type').remove();
+//   //   // }
+//   // }
+// }
