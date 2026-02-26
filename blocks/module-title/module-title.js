91102da4290b9f9ad17d728e1687711926d5c51f
lvss-33 <shanshan.lv@accenture.cn>
2026-02-26 17:20:16 +0800

    feat: module title show two, del text align json (#358)

--- blocks/module-title/module-title.js
+++ blocks/module-title/module-title.js
@@ -6,15 +6,11 @@ export default async function decorate(block) {
 
   // module title text align type
 
-  const textAlignTypeEl = document.querySelector('.text-align-type p');
+  // const textAlignTypeEl = document.querySelector('.text-align-type p');
 
-  if (textAlignTypeEl) {
-    const textAlignType = document.querySelector('.text-align-type p').innerHTML;
-    document.querySelector('.text-align-type').parentElement.classList.add(textAlignType);
-    document.querySelector('.text-align-type').remove();
-    // if (textAlignType) {
-    //   document.querySelector('.text-align-type').parentElement.classList.add(textAlignType);
-    //   document.querySelector('.text-align-type').remove();
-    // }
-  }
+  // if (textAlignTypeEl) {
+  //   const textAlignType = document.querySelector('.text-align-type p').innerHTML;
+  //   document.querySelector('.text-align-type').parentElement.classList.add(textAlignType);
+  //   document.querySelector('.text-align-type').remove();
+  // }
 }
