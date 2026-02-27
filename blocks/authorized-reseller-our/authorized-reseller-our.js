eb29c56a6d3198550a8ab89eece4ac79adbb4f46
HONGJIAN61\Hongjian <924266645@qq.com>
2026-02-27 11:58:47 +0800

    authorized-reseller-our init

---
+++ blocks/authorized-reseller-our/authorized-reseller-our.js
@@ -0,0 +1,26 @@
+export default function decorate(block) {
+  try {
+    const elementItems = [...block.children];
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('ar-note-header-text');
+
+    elementItems.forEach((element, index) => {
+      if (index === 0) {
+        element?.classList.add('ar-note-header-icon');
+      } else if (index === 1) {
+        element?.classList.add('ar-note-header-title');
+        textContainer.appendChild(element);
+      } else if (index === 2) {
+        element?.classList.add('ar-note-header-content');
+        textContainer.appendChild(element);
+      }
+    });
+    if (textContainer.children.length > 0) {
+      block.appendChild(textContainer);
+    }
+  } catch (error) {
+    /* eslint-disable-next-line no-console */
+    console.error('Authorized Reseller Our block decoration error:', error);
+  }
+}
