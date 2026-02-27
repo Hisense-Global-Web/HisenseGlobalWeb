05e1c46caab89a322797be4957c5f1039be99182
lvss-33 <shanshan.lv@accenture.cn>
2026-02-27 10:37:45 +0800

    feat: bug fix 442, del multi popup module code (#364)

--- blocks/popup-module/popup-module.js
+++ blocks/popup-module/popup-module.js
@@ -1,13 +1,4 @@
 export default function decorate(block) {
-  // console.log(block, 'section-popup-block');
-  // create popup mask
-  // const popupMaskDom = document.querySelector('.popup-mask');
-  // if (!popupMaskDom) {
-  //   const popupMaskEl = document.createElement('div');
-  //   popupMaskEl.className = 'popup-mask';
-  //   block.parentNode.append(popupMaskEl);
-  // }
-
   // create close button
   const closeDivEl = document.createElement('div');
   closeDivEl.className = 'popup-close';
@@ -15,14 +6,12 @@ export default function decorate(block) {
   closeImgEl.src = '/content/dam/hisense/us/common-icons/close-50.svg';
   closeImgEl.alt = 'Close Image';
   closeDivEl.append(closeImgEl);
-  // block.appendChild(closeDivEl);
 
   // close popup
   closeDivEl.addEventListener('click', () => {
     document.querySelectorAll('.popup-module-container').forEach((popupItem) => {
       popupItem.classList.remove('popup-show');
     });
-    // document.querySelector('.popup-mask').classList.remove('popup-mask-show');
     document.body.style.overflow = 'auto';
   });
 
@@ -30,7 +19,6 @@ export default function decorate(block) {
   const popupContentDom = document.createElement('div');
   popupContentDom.className = 'popup-content-container';
   block.appendChild(popupContentDom);
-  // console.log(block, 'block');
   [...block.children].forEach((row) => {
     if (!row.classList.contains('popup-close') && !row.classList.contains('popup-content-container')) {
       popupContentDom.appendChild(row);
@@ -48,10 +36,7 @@ export default function decorate(block) {
   || block.hasAttribute('data-aue-type')
   || block.closest('[data-aue-resource]')
   || block.closest('[data-aue-type]');
-  if (isEditorMode) {
-    // 编辑模式下， 找到包裹页面iframe 的 类名为 EditableOverlays 的 main 元素，将其设置为overflow:hidden，避免编辑器页面滚动导致的popup滚动问题
-    block.closest('.EditableOverlays').style.overflow = 'hidden';
-  } else {
+  if (!isEditorMode) {
     // 非编辑模式下隐藏所有 popup 元素
     document.querySelectorAll('.popup-module-container').forEach((popupItem) => {
       popupItem.classList.remove('popup-show');
