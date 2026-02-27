d903f4ecdff2645383259ac4eda5bbe1e34b8a00
Lv <shanshan.lv@accenture.cn>
2026-02-27 09:48:03 +0800

    feat: card grid show three, change json config, index set class name

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -7,13 +7,29 @@ export default async function decorate(block) {
     const itemTextBoxEl = document.createElement('div');
     itemTextBoxEl.className = 'item-text-box';
     [...child.children].forEach((item, itemIndex) => {
-      if (itemIndex > 0) {
-        const type = item.firstElementChild?.textContent;
-        item.classList.add(type);
-        // item.firstElementChild.remove();
-        itemTextBoxEl.append(item);
-      } else {
-        item.className = 'item-img-box';
+      // if (itemIndex > 0) {
+      //   const type = item.firstElementChild?.textContent;
+      //   item.classList.add(type);
+      //   // item.firstElementChild.remove();
+      //   itemTextBoxEl.append(item);
+      // } else {
+      //   item.className = 'item-img-box';
+      // }
+      switch (itemIndex) {
+        case 0:
+          item.className = 'item-img-box';
+          break;
+        case 1:
+          item.classList.add('item-subtitle');
+          itemTextBoxEl.append(item);
+          break;
+        case 2:
+          item.classList.add('item-title');
+          itemTextBoxEl.append(item);
+          break;
+        default:
+          item.classList.add('item-text');
+          itemTextBoxEl.append(item);
       }
     });
 
