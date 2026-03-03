41d0a6f5a81acc76a725552171c69546fd3c9ea2
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-03-03 14:46:54 +0800

    update fixbug

--- blocks/exception-page/exception-page.js
+++ blocks/exception-page/exception-page.js
@@ -1,25 +1,24 @@
-import { readBlockConfig } from "../../scripts/aem.js";
-export default function decorate(block) {
-    const config = readBlockConfig(block);
-    const [icon, title, description, btnText, btnLink] = block.children;
-    console.log(config);
-    
-    // add className
-    [icon, title, description, btnText, btnLink].forEach(d => {
-        const key = d.children[0].textContent.trim();
-        console.log(Object.keys(config).includes(key.toLowerCase()));
-        
-        if(key && Object.keys(config).includes(key.toLowerCase())) {
-            d.classList.add(key);
-            d.children[0].remove();
-        }
-    })
+import { readBlockConfig } from '../../scripts/aem.js';
 
-    const textArea = document.createElement('div');
-    textArea.className = 'textArea';
-    textArea.append(title, description);
-    block.append(textArea);
-    btnLink.querySelector('a').innerText = btnText.innerText.trim();
-    btnText.remove();
-    block.append(btnLink);
+export default function decorate(block) {
+  const config = readBlockConfig(block);
+  const [icon, title, description, btnText, btnLink] = block.children;
+
+  // add className
+  [icon, title, description, btnText, btnLink].forEach((d) => {
+    const key = d.children[0].textContent.trim();
+
+    if (key && Object.keys(config).includes(key.toLowerCase())) {
+      d.classList.add(key);
+      d.children[0].remove();
+    }
+  });
+
+  const textArea = document.createElement('div');
+  textArea.className = 'text-area';
+  textArea.append(title, description);
+  block.append(textArea);
+  btnLink.querySelector('a').innerText = btnText.innerText.trim();
+  btnText.remove();
+  block.append(btnLink);
 }
