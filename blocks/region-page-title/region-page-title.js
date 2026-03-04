d31b0e21542808752019090d2604838778375221
HONGJIAN61\Hongjian <924266645@qq.com>
2026-03-04 15:21:45 +0800

    region-page-title INIT

---
+++ blocks/region-page-title/region-page-title.js
@@ -0,0 +1,26 @@
+export default function decorate(block) {
+  try {
+    // const elementItems = [...block.children];
+
+    // const textContainer = document.createElement('div');
+    // textContainer.classList.add('ar-note-text');
+
+    // elementItems.forEach((element, index) => {
+    //   if (index === 0) {
+    //     element?.classList.add('ar-note-icon');
+    //   } else if (index === 1) {
+    //     element?.classList.add('ar-note-title');
+    //     textContainer.appendChild(element);
+    //   } else if (index === 2) {
+    //     element?.classList.add('ar-note-content');
+    //     textContainer.appendChild(element);
+    //   }
+    // });
+    // if (textContainer.children.length > 0) {
+    //   block.appendChild(textContainer);
+    // }
+  } catch (error) {
+    /* eslint-disable-next-line no-console */
+    console.error('Authorized Reseller Note block decoration error:', error);
+  }
+}
