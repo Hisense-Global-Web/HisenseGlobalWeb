3bf44693cd91b13d5802109ff0eeaccdb63bcafb
guoli.zhang <guoli.zhang@accenture.cn>
2026-03-04 10:06:37 +0800

    feat: test author page.

--- blocks/support-module-title/support-module-title.js
+++ blocks/support-module-title/support-module-title.js
@@ -1,28 +1,24 @@
-import { readBlockConfig } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   try {
-    const config = readBlockConfig(block);
-    const textStyle = config['txt-style'] || 'left';
-    const isCenter = textStyle === 'center';
-    const [textStyleEl, titleEl, subtitleEl] = block.querySelectorAll('p');
-    textStyleEl?.remove();
-    titleEl.classList.add('support-module-title-title');
-    if (isCenter) {
-      titleEl.classList.add('text-center');
-    }
-    // If subtitle exists
-    if (subtitleEl) {
-      // Check if subtitle is empty
-      if (subtitleEl?.innerHTML?.trim() === '') {
-        subtitleEl.remove();
-      } else {
-        subtitleEl.classList.add('support-module-title-subtitle');
-        if (isCenter) {
-          subtitleEl.classList.add('text-center');
-        }
-      }
-    }
+    const isCenter = true;
+    // const [textStyleP, titleP, subtitleP] = block.querySelectorAll('p');
+    // titleP.classList.add('support-module-title-title');
+    // if (isCenter) {
+    //   titleP.classList.add('text-center');
+    // }
+    // // If subtitle exists
+    // if (subtitleP) {
+    //   // Check if subtitle is empty
+    //   if (subtitleP?.innerHTML?.trim() === '') {
+    //     subtitleP.remove();
+    //   } else {
+    //     subtitleP.classList.add('support-module-title-subtitle');
+    //     if (isCenter) {
+    //       subtitleP.classList.add('text-center');
+    //     }
+    //   }
+    // }
+    // textStyleP?.remove();
   } catch (error) {
     /* eslint-disable-next-line no-console */
     console.error('Section Title block decoration error:', error);
