256378716501ebc534b64576c2a71e3015fe442f
ruoqiong.xiao <ruoqiong.xiao@accenture.com>
2026-03-04 20:53:55 +0800

    fix eslint

--- utils/wrap-in-richtext.js
+++ utils/wrap-in-richtext.js
@@ -1,19 +1,19 @@
 export default function wrapInRichtext(element) {
-	// if the element has no children or it's specifically an <li>,
-	// operate on its innerHTML.  <li> tags may already contain
-	// <br> elements, so we can't rely on textContent alone.
-	if (!element.children.length || element.tagName === 'LI') {
-		const html = element.innerHTML;
+  // if the element has no children or it's specifically an <li>,
+  // operate on its innerHTML.  <li> tags may already contain
+  // <br> elements, so we can't rely on textContent alone.
+  if (!element.children.length || element.tagName === 'LI') {
+    const html = element.innerHTML;
 
-		// replace every literal "/n" sequence with a <br> element.
-		// existing <br> tags are preserved, and if the li already
-		// contains both <br> and "/n" the latter will simply be
-		// converted without affecting the former.
-		if (html.includes('/n')) {
-			element.innerHTML = html.replace(/\/n/g, '<br>');
-		}
-	} else {
-		// recursively process children
-		[...element.children].forEach(child => wrapInRichtext(child));
-	}
+    // replace every literal "/n" sequence with a <br> element.
+    // existing <br> tags are preserved, and if the li already
+    // contains both <br> and "/n" the latter will simply be
+    // converted without affecting the former.
+    if (html.includes('/n')) {
+      element.innerHTML = html.replace(/\/n/g, '<br>');
+    }
+  } else {
+    // recursively process children
+    [...element.children].forEach((child) => wrapInRichtext(child));
+  }
 }
