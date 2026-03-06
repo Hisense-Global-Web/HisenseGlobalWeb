256b321b900687d1410fffbb3714973948f7b8c9
avalonXH <hang.xiao@accenture.com>
2026-03-06 18:00:00 +0800

    lint

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -212,7 +212,6 @@ function parseDropdownBtns(col) {
   if (!col) return [];
 
   const results = [];
-  console.log(col);
   const subMenuLinks = col.querySelectorAll('.sub-menu-link');
   if (subMenuLinks.length > 0) {
     subMenuLinks.forEach((subMenuLink) => {
@@ -634,7 +633,6 @@ const handleChangeNavPosition = (navigation) => {
 export default async function decorate(block) {
   const navPath = getFragmentPath('nav');
   const fragment = await loadFragment(navPath);
-  console.log(block, fragment);
   // 解析原始DOM
   const logo = parseLogo(fragment);
   const navItems = parseNavItems(fragment);
