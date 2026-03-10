b821b4cf167513b4f624b7525ec3d2d4ef87727d
lvss-33 <shanshan.lv@accenture.cn>
2026-03-10 14:04:17 +0800

    feat: compare bx error tolerant (#513)

--- blocks/product-card/product-card.js
+++ blocks/product-card/product-card.js
@@ -759,7 +759,7 @@ export default function decorate(block) {
         // card 中的 【Compare】 btn 是否添加 选中类
         compareEl.classList.toggle('compare-checked');
         // 当前商品选中属性
-        const curCardSelectedProperty = compareE.currentTarget.getAttribute('data-selected-property');
+        const curCardSelectedProperty = compareE.currentTarget.getAttribute('data-selected-property') ?? '';
         // 当前商品选中属性，对应的数据源（也是比较商品的数据来源）
         let cardSelectedVariant = sizeToVariant.get(curCardSelectedProperty) || selectedVariant || item;
         // 只有在没有 size 时，才回退使用 color
