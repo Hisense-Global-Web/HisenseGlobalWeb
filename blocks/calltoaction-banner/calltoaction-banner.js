c3bf2903dfa691405c6412665dc124e3f62a3392
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-03-16 14:58:37 +0800

    update block style and backgroundImage

--- blocks/calltoaction-banner/calltoaction-banner.js
+++ blocks/calltoaction-banner/calltoaction-banner.js
@@ -1,12 +1,26 @@
 export default async function decorate(block) {
     const containerDiv = document.createElement('div');
     containerDiv.className = 'banner-container h-grid-container';
+    
+    const updateBackgroundImage = () => {
+        const pcImg = block.dataset.pcImg;
+        const mobileImg = block.dataset.mobileImg;
+        if(window.innerWidth < 860) {
+            block.style.backgroundImage = `url(${mobileImg})`;
+        } else {
+            block.style.backgroundImage = `url(${pcImg})`;
+        }
+    };
+    
     [...block.children].forEach((child, index)=>{
         switch(index) {
             case 0:
                 child.className = 'banner-img';
-                block.style.backgroundImage = `url(${child.querySelector('img').src})`;
-                child.remove();    
+                const [pcImg, mobileImg] = child.querySelectorAll('img');
+                block.dataset.pcImg = pcImg.src;
+                block.dataset.mobileImg = mobileImg.src;
+                updateBackgroundImage(); // 初始设置
+                child.remove();
                 break;
             case 1:
                 child.className = 'banner-title';
@@ -22,5 +36,9 @@ export default async function decorate(block) {
 
         }
     })
+    
+    // 监听窗口大小变化
+    window.addEventListener('resize', updateBackgroundImage);
+    
     block.append(containerDiv);
 }
