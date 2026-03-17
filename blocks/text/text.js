40da737a2b33d6d01ddd3fe9756b12b95c84a358
Lv <shanshan.lv@accenture.cn>
2026-03-17 15:29:23 +0800

    feat: js control multiselect none val

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -5,6 +5,20 @@ import getDynamicHeaderHeight from '../../utils/dynamic-computed-header-height.j
 export default async function decorate(block) {
   // 如果block 是main 元素第一个的话，需要动态设置header高度，为block添加padding-top
   getDynamicHeaderHeight(block);
+
+  // 在author 编辑模式下，当multiselect 为空值时，不显示 None
+  const isEditorMode = block.hasAttribute('data-aue-resource')
+  || block.hasAttribute('data-aue-type')
+  || block.closest('[data-aue-resource]')
+  || block.closest('[data-aue-type]');
+
+  if (isEditorMode) {
+    const multiNoneEl = document.querySelector('.HpsDkq_spectrum-Tags-container--empty [aria-label="Description Alignment Class Name Options"]');
+    if (multiNoneEl) {
+      multiNoneEl.style.display = 'none';
+    }
+  }
+
   if (isUniversalEditor()) {
     return;
   }
