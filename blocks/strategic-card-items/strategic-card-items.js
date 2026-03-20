2dd653e2db9e1ec7e5c66a0dc0be01805c508c60
ruoqiongXiao <ruoqiong.xiao@accenture.cn>
2026-03-20 16:11:59 +0800

    update new config

--- blocks/strategic-card-items/strategic-card-items.js
+++ blocks/strategic-card-items/strategic-card-items.js
@@ -18,9 +18,10 @@ export default function decorate(block) {
   [...block.children].forEach((child) => {
     child.className = 'strategic-card-item';
     if (!child.children.length) return;
-    const [iconDiv, textDiv, btnDiv] = child.children;
+    const [iconDiv, textDiv, bodyCopy, btnDiv] = child.children;
     iconDiv.className = 'card-icon';
     textDiv.className = 'card-text';
+    bodyCopy.className = "card-description";
     btnDiv.className = 'card-btn';
     if (btnDiv && !btnDiv.textContent.trim()) {
       btnDiv.style.display = 'none';
@@ -32,6 +33,7 @@ export default function decorate(block) {
     containerDiv.append(child);
   });
   block.replaceChildren(containerDiv);
+  if (!block.querySelector('.btn-label')) return;
   whenElementReady('.strategic-card-items', () => {
     bindEvent(block);
   });
