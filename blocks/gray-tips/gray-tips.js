1779419f3593c9c872c5b1c96dcb24bef314f9f8
guoli.zhang <guoli.zhang@accenture.cn>
2026-04-01 14:51:13 +0800

    feat: add gray tips.

--- blocks/gray-tips/gray-tips.js
+++ blocks/gray-tips/gray-tips.js
@@ -1,2 +1,13 @@
-export default function decorate() {
+export default function decorate(block) {
+  const [title, subtitle] = [...block.children];
+  if (title?.querySelector('p')?.textContent?.length > 0) {
+    title.classList.add('title');
+  } else {
+    title?.remove();
+  }
+  if (subtitle?.querySelector('p')?.textContent?.length > 0) {
+    subtitle.classList.add('subtitle');
+  } else {
+    subtitle?.remove();
+  }
 }
