5dcfc9869979dbcea86dc8af56be0c52eeb1f8f9
avalonXH <hang.xiao@accenture.com>
2026-04-01 18:03:32 +0800

    1

--- blocks/collapse-list/collapse-list.js
+++ blocks/collapse-list/collapse-list.js
@@ -1,3 +1,53 @@
 export default function decorate(block) {
-  console.log(block);
+  const segments = window.location.pathname.split('/').filter(Boolean);
+  const country = segments[segments[0] === 'content' ? 2 : 0] || '';
+  const isEditMode = block.hasAttribute('data-aue-resource');
+  if (isEditMode) {
+    return;
+  }
+  [...block.children].forEach((card) => {
+    const type = card.firstElementChild?.textContent?.trim() || '';
+    card.classList.add('collapse-card');
+    let showButton = false;
+    console.log(type);
+    [...card.children].forEach((row, index) => {
+      console.log(row, index);
+      if (index === 0) {
+        row.classList.add('collapse-title');
+        const icon = document.createElement('img');
+        icon.src = `/content/dam/hisense/${country}/common-icons/chevron-up.svg`;
+        icon.alt = '';
+        icon.className = 'chevron';
+        icon.addEventListener('click', (e) => {
+          e.stopPropagation();
+          const collapseCard = e.currentTarget.closest('.collapse-card');
+          if (collapseCard) {
+            collapseCard.classList.toggle('hide');
+          }
+        });
+        row.appendChild(icon);
+      } else if (index === 1) {
+        row.classList.add('collapse-context');
+      } else if (index === 2) {
+        if (row.textContent.trim() === 'true') {
+          showButton = true;
+        }
+        row.remove();
+      } else if (index === 3) {
+        const btnEl = document.createElement('div');
+        btnEl.classList.add('collapse-btn');
+        if (showButton) {
+          btnEl.textContent = row.children[0].textContent;
+          btnEl.addEventListener('click', () => {
+            window.location.href = row.children[1].querySelector('a').href;
+          });
+        } else {
+          btnEl.classList.add('hide');
+        }
+        const contextEl = card.querySelector('.collapse-context');
+        contextEl.appendChild(btnEl);
+        row.remove();
+      }
+    });
+  });
 }
