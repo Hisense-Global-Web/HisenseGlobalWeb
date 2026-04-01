9b164f41f4dce4cd752c88a2a16be0c767745b58
avalonXH <hang.xiao@accenture.com>
2026-04-01 11:03:10 +0800

    css

--- blocks/adaptive-images/adaptive-images.js
+++ blocks/adaptive-images/adaptive-images.js
@@ -1,1 +1,13 @@
-export default function decorate() {}
+export default function decorate(block) {
+  const isEditMode = block.hasAttribute('data-aue-resource');
+  if (isEditMode) {
+    return;
+  }
+  [...block.children].forEach((row, index) => {
+    if (index === 0) {
+      row.classList.add('pc-image');
+    } else if (index === 1) {
+      row.classList.add('mobile-image');
+    }
+  });
+}
