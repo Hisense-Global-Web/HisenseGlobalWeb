46ff60c4a0b80f6c17617025054ff7d7a0938458
qi.b.zhang <qi.b.zhang@accenture.cn>
2026-04-03 17:48:18 +0800

    fix bugs

--- blocks/store-information-card/store-information-card.js
+++ blocks/store-information-card/store-information-card.js
@@ -1,26 +1,20 @@
-const generateCard = (info) => {
-  info?.classList?.add?.('list-card');
-  // // eslint-disable-next-line no-unsafe-optional-chaining
-  // const [ infoEL, titleEL, textEL, text2, location]= info?.children ?? []
-};
-
-/**
- * Information List Module Block
- */
 export default function decorate(block) {
   // const config = readBlockConfig(block);
   const [stage, category, ...infoList] = [...block.children];
-  stage?.parentNode?.parentNode?.setAttribute('data-tag', stage.firstElementChild.textContent?.trim());
-  category.parentNode?.parentNode?.setAttribute('data-item-tag', category.firstElementChild.textContent?.trim());
-  stage.className = 'card-name';
+  stage?.parentNode?.parentNode?.setAttribute('data-tag', stage.lastElementChild.textContent?.trim());
+  category.parentNode?.parentNode?.setAttribute('data-item-tag', category.lastElementChild.textContent?.trim());
   stage?.remove?.();
   category?.remove?.();
-  const wrapper = document.createElement('div');
-  wrapper.className = 'list-card-container';
+  // const wrapper = document.createElement('div');
+  // wrapper.className = 'list-card-container';
   infoList?.forEach((info) => {
-    generateCard(info);
-    wrapper.append(info);
+    if (info.children.length > 1) {
+      info.className = info.firstElementChild.textContent?.trim();
+      info.firstElementChild?.remove?.();
+    } else {
+      info.className = 'sever-item';
+    }
+    // wrapper.append(info);
   });
-  block.replaceChildren(stage, wrapper);
-  block.classList.add('loaded');
+  // block.replaceChildren(wrapper);
 }
