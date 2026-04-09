78f568dced010f84268159a9fa696b7fb5d0f154
guoli.zhang <guoli.zhang@accenture.cn>
2026-04-09 16:29:06 +0800

    feat: update recall info text.

--- blocks/recall-text-info/recall-text-info.js
+++ blocks/recall-text-info/recall-text-info.js
@@ -1,2 +1,16 @@
-export default function decorate() {
+export default function decorate(block) {
+  const recallInfoList = [...block.children] ?? [];
+  console.log('recallInfoList', recallInfoList);
+  if (recallInfoList?.length) {
+    recallInfoList.forEach((recallInfo) => {
+      recallInfo.classList.add('recall-text-info-item');
+      const [title, content] = recallInfo?.children ?? [];
+      if (title) {
+        title.classList.add('title');
+      }
+      if (content) {
+        content.classList.add('content');
+      }
+    });
+  }
 }
