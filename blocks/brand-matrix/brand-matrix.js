2e6c56861703e935e3e541cb629393c43dbbd267
Lv <shanshan.lv@accenture.cn>
2026-04-09 14:33:34 +0800

    feat: add brand matrix block

---
+++ blocks/brand-matrix/brand-matrix.js
@@ -0,0 +1,5 @@
+export default async function decorate(block) {
+  console.log('decorate brand-matrix');
+  const brandMatrixWrapperEl = document.createElement('div');
+  brandMatrixWrapperEl.className = 'brand-matrix-wrapper';
+}
