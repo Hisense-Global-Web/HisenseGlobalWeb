bb9bcbdfcaae878cc1d8d4be78d6c4b7d76ef67a
Lv <shanshan.lv@accenture.cn>
2026-04-09 15:07:39 +0800

    feat: brand matrix block style change

--- blocks/brand-matrix/brand-matrix.js
+++ blocks/brand-matrix/brand-matrix.js
@@ -1,5 +1,24 @@
 export default async function decorate(block) {
-  console.log('decorate brand-matrix');
   const brandMatrixWrapperEl = document.createElement('div');
-  brandMatrixWrapperEl.className = 'brand-matrix-wrapper';
+  brandMatrixWrapperEl.className = 'brand-matrix-container';
+  [...block.children].forEach((child) => {
+    child.className = 'brand-matrix-item';
+    brandMatrixWrapperEl.append(child);
+    const row = [...child.children];
+    const itemBackVal = row[0].textContent.split(',')[1]?.trim() || 'gradient-val';
+    child.classList.add(itemBackVal);
+    row.forEach((item, itemIndex) => {
+      switch (itemIndex) {
+        case 0:
+          item.remove();
+          break;
+        case 1:
+          item.className = 'brand-logo';
+          break;
+        default:
+          break;
+      }
+    });
+  });
+  block.append(brandMatrixWrapperEl);
 }
