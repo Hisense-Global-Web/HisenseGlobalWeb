12229ee6e22908534f7740f9a72ba14dd6e4b11b
qi.b.zhang <qi.b.zhang@accenture.cn>
2026-04-10 11:37:13 +0800

    fix bugs

--- blocks/store-information-card/store-information-card.js
+++ blocks/store-information-card/store-information-card.js
@@ -1,20 +1,23 @@
 export default function decorate(block) {
   // const config = readBlockConfig(block);
-  const [stage, category, ...infoList] = [...block.children];
-  stage?.parentNode?.parentNode?.setAttribute('data-tag', stage.lastElementChild.textContent?.trim());
-  category.parentNode?.parentNode?.setAttribute('data-item-tag', category.lastElementChild.textContent?.trim());
-  stage?.remove?.();
-  category?.remove?.();
+  const [stage, ...infoList] = [...block.children];
   // const wrapper = document.createElement('div');
   // wrapper.className = 'list-card-container';
+  const cates = [];
   infoList?.forEach((info) => {
     if (info.children.length > 1) {
       info.className = info.firstElementChild.textContent?.trim();
       info.firstElementChild?.remove?.();
     } else {
       info.className = 'sever-item';
+      const [cate, img] = [...info.firstElementChild.children];
+      img.className = 'serve-icon';
+      cates.push(cate?.textContent);
     }
-    // wrapper.append(info);
   });
+  const uniqueArr = [...new Set(cates)];
+  stage?.parentNode?.parentNode?.setAttribute('data-tag', stage.lastElementChild.textContent?.trim());
+  stage.parentNode?.parentNode?.setAttribute('data-item-tag', uniqueArr.join(','));
+  stage?.remove?.();
   // block.replaceChildren(wrapper);
 }
