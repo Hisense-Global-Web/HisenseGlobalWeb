75150561fffe0133ae9e7307a835524e88f3f298
Lv <shanshan.lv@accenture.cn>
2026-04-13 10:34:50 +0800

    feat: test add fifa banner

---
+++ blocks/fifa-banner/fifa-banner.js
@@ -0,0 +1,10 @@
+export default function decorate(block) {
+  console.log('decorate fifa banner', block);
+  // [...block.children].forEach((row, idx) => {
+  //   if (idx === 0) {
+  //     row.className = 'pc-box-img';
+  //   } else {
+  //     row.className = 'mobile-box-img';
+  //   }
+  // });
+}
