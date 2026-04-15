899a02b4397aef812c6906e5454898258db6eac4
avalonXH <hang.xiao@accenture.com>
2026-04-15 10:02:16 +0800

    ff

--- blocks/campaign-product/campaign-product.js
+++ blocks/campaign-product/campaign-product.js
@@ -1,3 +1,24 @@
 export default function decorate(block) {
   console.log(block);
+  const data = [];
+  [...block.children].forEach((row) => {
+    row.classList.add('campaign-category');
+    const category = {
+      name: '',
+      src: '',
+      products: [],
+    };
+    [...row.children].forEach((item, index) => {
+      if (index === 0) {
+        const imgEl = item.querySelector('img');
+        category.src = imgEl?.src;
+      } else if (index === 1) {
+        category.name = item.textContent.trim();
+      } else if (index === 2) {
+        
+      }
+    });
+    data.push(category);
+    console.log(data);
+  });
 }
