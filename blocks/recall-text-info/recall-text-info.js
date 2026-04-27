0abb696f5c88519c6e9fe02f18b1dc08fb2734f2
guoli.zhang <guoli.zhang@accenture.cn>
2026-04-27 14:51:54 +0800

    feat: add recall text info.

--- blocks/recall-text-info/recall-text-info.js
+++ blocks/recall-text-info/recall-text-info.js
@@ -1,6 +1,5 @@
 export default function decorate(block) {
   const recallInfoList = [...block.children] ?? [];
-  console.log('recallInfoList', recallInfoList);
   if (recallInfoList?.length) {
     recallInfoList.forEach((recallInfo) => {
       recallInfo.classList.add('recall-text-info-item');
