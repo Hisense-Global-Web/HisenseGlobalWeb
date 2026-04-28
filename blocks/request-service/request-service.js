6429060f8b36741f5c2237ec519590be730df07c
guoli.zhang <guoli.zhang@accenture.cn>
2026-04-28 17:09:52 +0800

    feat: add request service.

---
+++ blocks/request-service/request-service.js
@@ -0,0 +1,15 @@
+export default function decorate(block) {
+  const requestServiceList = [...block.children] ?? [];
+  if (requestServiceList?.length) {
+    requestServiceList.forEach((recallInfo) => {
+      recallInfo.classList.add('recall-text-info-item');
+      const [title, content] = recallInfo?.children ?? [];
+      if (title) {
+        title.classList.add('title');
+      }
+      if (content) {
+        content.classList.add('content');
+      }
+    });
+  }
+}
