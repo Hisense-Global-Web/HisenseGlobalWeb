6af74a4d641a4e6ce70db667abd379ec8d9ecc26
guoli.zhang <guoli.zhang@accenture.cn>
2026-04-29 11:44:33 +0800

    feat: update request service.

--- blocks/request-service/request-service.js
+++ blocks/request-service/request-service.js
@@ -1,14 +1,33 @@
 export default function decorate(block) {
   const requestServiceList = [...block.children] ?? [];
   if (requestServiceList?.length) {
-    requestServiceList.forEach((recallInfo) => {
-      recallInfo.classList.add('recall-text-info-item');
-      const [title, content] = recallInfo?.children ?? [];
-      if (title) {
-        title.classList.add('title');
+    requestServiceList.forEach((recallInfo, index) => {
+      recallInfo.classList.add('request-service-item');
+      const [textContentEl, buttonEl, buttonLinkEl] = recallInfo?.children ?? [];
+      textContentEl.classList.add('title-wrapper');
+      const [titleEl, contentEl] = textContentEl?.children ?? [];
+      if (titleEl) {
+        titleEl.classList.add('title');
       }
-      if (content) {
-        content.classList.add('content');
+      if (contentEl) {
+        contentEl.classList.add('content');
+      }
+      const buttonLink = buttonLinkEl.querySelector('a')?.textContent ?? null;
+      buttonLinkEl?.remove();
+      if (!buttonEl?.children?.length) {
+        buttonEl?.remove();
+      } else {
+        buttonEl.classList.add('button');
+        if (buttonLink) {
+          buttonEl.addEventListener('click', () => {
+            window.location.href = buttonLink;
+          });
+        }
+      }
+      if (index < requestServiceList.length - 1) {
+        const divideLineEl = document.createElement('div');
+        divideLineEl.className = 'divide-line';
+        recallInfo.after(divideLineEl);
       }
     });
   }
