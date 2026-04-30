406f08f125707281dce4c739fb3e062b27e7947a
Lv <shanshan.lv@accenture.cn>
2026-04-30 16:55:45 +0800

    feat: minimalist hero banner background-image name change dom js logic change

--- blocks/minimalist-hero-banner/minimalist-hero-banner.js
+++ blocks/minimalist-hero-banner/minimalist-hero-banner.js
@@ -1,27 +1,24 @@
 import getDynamicHeaderHeight from '../../utils/dynamic-computed-header-height.js';
 import { readBlockConfig } from '../../scripts/aem.js';
+
 export default async function decorate(block) {
   const textContainer = document.createElement('div');
   const config = readBlockConfig(block);
   console.log(config, 'config');
-  // console.log(block, 'blockkkk');
   [...block.children].forEach((child) => {
     if (child.querySelector('picture')) child.setAttribute('class', 'banner-image');
     else {
-      // console.log(child, 'ccccc');
-      textContainer.append(...child.firstElementChild.children);
+      const key = child.children[0].textContent.trim();
+      if (key && Object.keys(config).includes(key.toLowerCase())) {
+        child.classList.add(key);
+        child.children[0].remove();
+      }
+      textContainer.append(child);
       textContainer.setAttribute('class', 'text-container h-grid-container');
-      block.replaceChild(textContainer, child);
-      // const pAll = textContainer.querySelectorAll('p');
-      // pAll.forEach((p, pIdx) => {
-      //   if (pIdx === 0) {
-      //     p.setAttribute('class', 'minimalist-title');
-      //   } else {
-      //     p.setAttribute('class', 'minimalist-subtitle');
-      //   }
-      // });
+      // block.replaceChild(textContainer, child);
     }
   });
+  block.append(textContainer);
 
   getDynamicHeaderHeight(block);
 }
