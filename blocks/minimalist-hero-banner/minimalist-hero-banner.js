cb4eebe80abb27c190cb8daff8201c1fb4173379
Lv <shanshan.lv@accenture.cn>
2026-04-30 14:53:20 +0800

    feat: minimalist hero banner json change del value epmty

--- blocks/minimalist-hero-banner/minimalist-hero-banner.js
+++ blocks/minimalist-hero-banner/minimalist-hero-banner.js
@@ -2,12 +2,22 @@ import getDynamicHeaderHeight from '../../utils/dynamic-computed-header-height.j
 
 export default async function decorate(block) {
   const textContainer = document.createElement('div');
+  // console.log(block, 'blockkkk');
   [...block.children].forEach((child) => {
     if (child.querySelector('picture')) child.setAttribute('class', 'banner-image');
     else {
+      // console.log(child, 'ccccc');
       textContainer.append(...child.firstElementChild.children);
       textContainer.setAttribute('class', 'text-container h-grid-container');
       block.replaceChild(textContainer, child);
+      // const pAll = textContainer.querySelectorAll('p');
+      // pAll.forEach((p, pIdx) => {
+      //   if (pIdx === 0) {
+      //     p.setAttribute('class', 'minimalist-title');
+      //   } else {
+      //     p.setAttribute('class', 'minimalist-subtitle');
+      //   }
+      // });
     }
   });
 
