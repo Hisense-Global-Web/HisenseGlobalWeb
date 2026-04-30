abfcb08c1a9ff5f95d4049210393aec35cd888e4
Lv <shanshan.lv@accenture.cn>
2026-04-30 18:27:54 +0800

    feat: minimalist dom js logic change

--- blocks/minimalist-hero-banner/minimalist-hero-banner.js
+++ blocks/minimalist-hero-banner/minimalist-hero-banner.js
@@ -2,38 +2,22 @@ import getDynamicHeaderHeight from '../../utils/dynamic-computed-header-height.j
 import { readBlockConfig } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-  // const textContainer = document.createElement('div');
+  const textContainer = document.createElement('div');
   const config = readBlockConfig(block);
-  console.log(config, 'config');
   [...block.children].forEach((child) => {
-    console.log(child, 'child');
-    // if (child.querySelector('picture')) {
-    //   // child.setAttribute('class', 'banner-image');
-    //   // block.append(child);
-    // } else {
-    //   const key = child.children[0].textContent.trim();
-    //   if (key && Object.keys(config).includes(key.toLowerCase())) {
-    //     child.classList.add(key);
-    //     child.children[0].remove();
-    //   }
-    //   textContainer.append(child);
-    //   textContainer.setAttribute('class', 'text-container h-grid-container');
-    //   // block.replaceChild(textContainer, child);
-    // }
-    // const key = child.children[0].textContent.trim();
-    // if (key && Object.keys(config).includes(key.toLowerCase())) {
-    //   child.classList.add(key);
-    //   child.children[0].remove();
-    // }
-    // if (key === 'background-image') {
-    //   child.setAttribute('class', 'banner-image');
-    //   block.append(child);
-    // } else {
-    //   textContainer.append(child);
-    //   textContainer.setAttribute('class', 'text-container h-grid-container');
-    // }
+    const key = child.children[0].textContent.trim();
+    if (key && Object.keys(config).includes(key.toLowerCase())) {
+      child.classList.add(key);
+      child.children[0].remove();
+    }
+    if (key === 'background-image') {
+      child.setAttribute('class', 'banner-image');
+    } else {
+      textContainer.append(child);
+      textContainer.setAttribute('class', 'text-container h-grid-container');
+    }
   });
-  // block.append(textContainer);
+  block.append(textContainer);
 
   getDynamicHeaderHeight(block);
 }
