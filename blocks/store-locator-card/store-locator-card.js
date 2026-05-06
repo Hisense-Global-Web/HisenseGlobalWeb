30196eb959d80c87abf3d9f3a8dfa6a83fd64e08
guoli.zhang <guoli.zhang@accenture.cn>
2026-05-06 11:41:43 +0800

    fix: update stroe locator card.

--- blocks/store-locator-card/store-locator-card.js
+++ blocks/store-locator-card/store-locator-card.js
@@ -1,16 +1,51 @@
-import { readBlockConfig } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-  const config = readBlockConfig(block);
-  console.log('config', config);
-  // const [leabel1El, placeholder1El, label2El, placeholder2El, buttonTextEl, noResultEl, ...cityListEl] = [...block.children] ?? []
-  // const bottomWrapperEl = document.createElement('div');
-  // bottomWrapperEl.className = 'bottom-wrapper';
-
-  // if (cityListEl?.length) {
-  //   const cityList = [];
-  //   cityList.forEach((cityEl) => {
-  //     const [tagEl, cityNameEl, storeNameEl, label1El] = [...cityEl.children] ?? [];
-  //   });
-  // }
+  const [tagEl, cityStoreEl, contentWrapper1El, contentWrapper2El, label3El, ...typeList] = [...block.children] ?? [];
+  tagEl.className = 'tag';
+  if (cityStoreEl) {
+    cityStoreEl.className = 'city-store-wrapper';
+    const [cityEl, storeEl] = cityStoreEl?.querySelectorAll('p') ?? [];
+    cityEl?.classList?.add('city');
+    storeEl?.classList?.add('store');
+  }
+  const bottomWrapper = document.createElement('div');
+  bottomWrapper.className = 'bottom-wrapper';
+  if (contentWrapper1El) {
+    contentWrapper1El.className = 'content-wrapper';
+    const [title, content] = contentWrapper1El?.querySelectorAll('p') ?? [];
+    title?.classList?.add('title');
+    content?.classList?.add('content');
+    bottomWrapper.appendChild(contentWrapper1El);
+  }
+  if (contentWrapper2El) {
+    contentWrapper2El.className = 'content-wrapper';
+    const [title, content] = contentWrapper2El?.querySelectorAll('p') ?? [];
+    title?.classList?.add('title');
+    content?.classList?.add('content');
+    bottomWrapper.appendChild(contentWrapper2El);
+  }
+  if (label3El) {
+    label3El.className = 'title';
+    const typeWrapperEl = document.createElement('div');
+    typeWrapperEl.className = 'type-wrapper';
+    typeWrapperEl.appendChild(label3El);
+    const typeListWrapperEl = document.createElement('div');
+    typeListWrapperEl.className = 'type-list-wrapper';
+    if (typeList?.length) {
+      typeList.forEach((typeEl) => {
+        const iconTextWrapper = typeEl?.children?.[0];
+        if (iconTextWrapper) {
+          iconTextWrapper.className = 'icon-text-wrapper';
+          // eslint-disable-next-line no-unused-vars
+          const [icon, text] = [...iconTextWrapper.children] ?? [];
+          if (text) {
+            text.className = 'type-text';
+          }
+        }
+        typeListWrapperEl.appendChild(typeEl);
+      });
+    }
+    typeWrapperEl.appendChild(typeListWrapperEl);
+    bottomWrapper.appendChild(typeWrapperEl);
+  }
+  block.appendChild(bottomWrapper);
 }
