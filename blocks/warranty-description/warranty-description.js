92c2d262337f6b694c0817a65892aaa978625150
Lv <shanshan.lv@accenture.cn>
2026-05-07 17:22:39 +0800

    feat: warranty description js and style change

--- blocks/warranty-description/warranty-description.js
+++ blocks/warranty-description/warranty-description.js
@@ -1,3 +1,14 @@
 export default async function decorate(block) {
   console.log('decorate warranty description', block);
+  [...block.children].forEach((item, index) => {
+    if (index === 0) {
+      item.className = 'des-title';
+    } else if (index === 1) {
+      item.className = 'des-subtitle';
+    } else if (index === 2) {
+      item.className = 'des-body';
+    } else {
+      item.className = 'des-button';
+    }
+  });
 }
