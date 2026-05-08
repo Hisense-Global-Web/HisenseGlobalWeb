95ec214b1c6afbc990e6bb7aedd0aee1066ead27
Lv <shanshan.lv@accenture.cn>
2026-05-08 15:01:23 +0800

    feat: warranty description btn logic and style change

--- blocks/warranty-description/warranty-description.js
+++ blocks/warranty-description/warranty-description.js
@@ -1,5 +1,6 @@
+import { handleCommonDownloadClick } from '../../utils/download.js';
+
 export default async function decorate(block) {
-  console.log('decorate warranty description', block);
   [...block.children].forEach((item, index) => {
     if (index === 0) {
       item.className = 'des-title';
@@ -8,14 +9,23 @@ export default async function decorate(block) {
     } else if (index === 2) {
       item.className = 'des-body';
     } else {
-      item.className = 'des-button';
-      console.log('item', item);
+      item.className = 'button-box';
       const pAll = item.querySelectorAll('p');
-      const btnLabel = pAll[1].textContent.trim();
+      pAll[1].className = 'des-button';
+      const btnLabel = pAll[1]?.textContent.trim();
       if (btnLabel) {
-        pAll[0].querySelector('a').textContent = btnLabel;
-        pAll[1].remove();
+        const downloadLink = pAll[0].querySelector('a')?.href || pAll[0].querySelector('img')?.src || '';
+        pAll[0].remove();
+        item.addEventListener('click', (e) => {
+          e.preventDefault();
+          handleCommonDownloadClick(downloadLink);
+        });
       }
     }
   });
+  const title = block.querySelector('.des-title')?.textContent.trim();
+  const subtitle = block.querySelector('.des-subtitle')?.textContent.trim();
+  if (title && subtitle) {
+    block.querySelector('.des-subtitle').classList.add('has-title');
+  }
 }
