f600f6773a3048c7564e2a5087c4f9cefb9e0dde
Lv <shanshan.lv@accenture.cn>
2026-05-09 15:14:48 +0800

    feat: warranty note accordion style and js change

--- blocks/warning-note/warning-note.js
+++ blocks/warning-note/warning-note.js
@@ -1,8 +1,8 @@
 export default function decorate(block) {
-  const isEditMode = block.hasAttribute('data-aue-resource');
-  if (isEditMode) {
-    return;
-  }
+  // const isEditMode = block.hasAttribute('data-aue-resource');
+  // if (isEditMode) {
+  //   return;
+  // }
   [...block.children].forEach((row, index) => {
     if (index === 0) {
       row.classList.add('note-title');
