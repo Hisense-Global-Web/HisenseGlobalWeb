a9abc463c191c53ee3f5cbbb3575864e848fee7b
guoli.zhang <guoli.zhang@accenture.cn>
2026-05-14 09:06:31 +0800

    fix: fix bug.

--- blocks/store-locator-card/store-locator-card.js
+++ blocks/store-locator-card/store-locator-card.js
@@ -1,11 +1,13 @@
-import { decorateStoreLocatorSection } from '../../scripts/aem.js';
+// import { decorateStoreLocatorSection } from '../../scripts/aem.js';
+import { loadSection } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const isEditMode = block.hasAttribute('data-aue-resource');
   if (isEditMode) {
     const storeLocatorSections = block.closest('.store-locator-container');
     if (storeLocatorSections) {
-      decorateStoreLocatorSection(storeLocatorSections);
+      // decorateStoreLocatorSection(storeLocatorSections);
+      loadSection(storeLocatorSections);
     }
   }
 }
