783d3c914951875858f877c86b61d2a53dd101bb
guoli.zhang <guoli.zhang@accenture.cn>
2026-05-14 14:26:50 +0800

    fix: fix bug.

--- blocks/store-locator-card/store-locator-card.js
+++ blocks/store-locator-card/store-locator-card.js
@@ -1,21 +1,24 @@
-// import { decorateStoreLocatorSection } from '../../scripts/aem.js';
+import { loadSections } from '../../scripts/aem.js';
 // import { loadSection } from '../../scripts/aem.js';
-import { generateStoreEl } from '../store-locator/store-locator.js';
+// import { generateStoreEl } from '../store-locator/store-locator.js';
+// import * as parentNode from '../store-locator/store-locator.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   const isEditMode = block.hasAttribute('data-aue-resource');
   if (isEditMode) {
-    const { node } = generateStoreEl(block);
-    const cloneNode = node?.cloneNode('true');
-    if (cloneNode) {
-      // block.append([...cloneNode.children]);
-      if (cloneNode?.children?.length) {
-        block.innerHTML = '';
-        [...cloneNode.children].forEach((child) => {
-          block.appendChild(child);
-        });
-      }
-    }
+    const main = document.querySelector('main');
+    await loadSections(main);
+    // const { node } = generateStoreEl(block);
+    // const cloneNode = node?.cloneNode('true');
+    // if (cloneNode) {
+    //   // block.append([...cloneNode.children]);
+    //   if (cloneNode?.children?.length) {
+    //     block.innerHTML = '';
+    //     [...cloneNode.children].forEach((child) => {
+    //       block.appendChild(child);
+    //     });
+    //   }
+    // }
     // const storeLocatorSections = block.closest('.store-locator-container');
     // if (storeLocatorSections) {
     //   // decorateStoreLocatorSection(storeLocatorSections);
