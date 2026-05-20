bcdeed4318c5709293939b1a416ea66c0982de9a
guoil-zhang <guoli.zhang@accenture.cn>
2026-05-20 20:30:49 +0800

    Zhanggl bug0520 (#884)
    * fix: fix bugs and update promotional text.

--- blocks/promotional-text/promotional-text.js
+++ blocks/promotional-text/promotional-text.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-  const [fontColorEl, textAlignEl, titleEl, subtitleEl] = [...block.children] ?? [];
+  const [fontColorEl, textAlignEl, titleEl, subtitleEl, contentEl] = [...block.children] ?? [];
   const fontColor = fontColorEl?.querySelector('p')?.textContent ?? 'green60';
   const textAlign = textAlignEl?.querySelector('p')?.textContent ?? 'align-center';
   block.classList.add(fontColor, textAlign);
@@ -10,5 +10,8 @@ export default function decorate(block) {
   }
   if (subtitleEl) {
     subtitleEl.classList.add('subtitle');
+  }
+  if (contentEl) {
+    contentEl.classList.add('content');
   }
 }
