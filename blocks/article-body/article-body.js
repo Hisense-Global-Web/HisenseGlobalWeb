c7862d29c5219bea5ff9dbb1c2a88497c73fff59
肖航 <hang.xiao@accenture.cn>
2026-05-28 15:29:17 +0800

    1111

--- blocks/article-body/article-body.js
+++ blocks/article-body/article-body.js
@@ -6,6 +6,33 @@ const country = segments[segments[0] === 'content' ? 2 : 0] || '';
 export default function decorate(block) {
   const isEditMode = block.hasAttribute('data-aue-resource');
   if (isEditMode) {
+    [...block.children].forEach((row) => {
+      const type = row.firstElementChild?.textContent?.trim() || '';
+      if (type === 'flexend-side-by-side') {
+        const GroupDiv = document.createElement('div');
+        GroupDiv.className = 'text-body-group-flexend';
+        // 图
+        const imgGroupDiv = document.createElement('div');
+        const imgEl = row.querySelector('img');
+        imgGroupDiv.className = 'text-body-img-group';
+        imgGroupDiv.append(imgEl);
+        // 文
+        const textGroupDiv = document.createElement('div');
+        const title = row.children[2] || '';
+        title.className = 'text-body-title';
+        const desc = row.children[3] || '';
+        desc.className = 'text-body-desc';
+        textGroupDiv.className = 'text-body-text-group';
+        textGroupDiv.append(title, desc);
+        if (imgEl) {
+          GroupDiv.append(imgGroupDiv);
+        }
+        GroupDiv.append(textGroupDiv);
+        console.log(GroupDiv)
+      } else {
+        console.log(row)
+      }
+    });
     return;
   }
   const ArticleBodyDiv = document.createElement('div');
