895ff2a50a7550bb5c57fd4e5dfa44f13b67aaaf
肖航 <hang.xiao@accenture.cn>
2026-06-01 11:22:32 +0800

    add company  translate

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -33,6 +33,7 @@ import {
 
 import { isAuthorHostname } from '../../scripts/environment.js';
 import { SCREEN_POINT } from '../../utils/constants.js';
+import translate from '../../utils/translate.js';
 
 const LOGOUT_TEXT = {
   en: {
@@ -1133,6 +1134,7 @@ const handleAccountActionClick = async (event) => {
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
+  const { language } = getLocaleFromPath();
   // clean nav page content in eds side
   if (!isAuthorHostname() && isNavPage()) {
     document.head.remove();
@@ -1344,7 +1346,7 @@ export default async function decorate(block) {
   navSecond.className = `nav-second h-grid-container ${isCompanyPage || isSupportPage ? '' : 'hidden'}`;
   const CompanyEl = document.createElement('div');
   CompanyEl.className = 'route-company';
-  CompanyEl.textContent = 'Company';
+  CompanyEl.textContent = translate('COMPANY', language);
   const CompanyGroupEl = document.createElement('div');
   CompanyGroupEl.className = 'company-group';
   company.forEach((item) => {
