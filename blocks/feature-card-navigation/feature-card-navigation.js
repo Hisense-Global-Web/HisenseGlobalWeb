4f7e26a0583e8e1f7a18d8e4dac1fe1065edcb32
Lv <shanshan.lv@accenture.cn>
2026-06-09 16:02:51 +0800

    feat: feature card navigation add step json and js css change on china site

--- blocks/feature-card-navigation/feature-card-navigation.js
+++ blocks/feature-card-navigation/feature-card-navigation.js
@@ -1,9 +1,15 @@
+import { getLocaleFromPath } from '../../scripts/locale-utils.js';
+
 export default async function decorate(block) {
   const cardNavigationWrapperEl = document.createElement('div');
   cardNavigationWrapperEl.className = 'card-navigation-wrapper';
+  const { language } = getLocaleFromPath();
 
   [...block.children].forEach((row) => {
     row.className = 'card-box';
+    if (language === 'zh') {
+      row.classList.add('zh-card-box');
+    }
     cardNavigationWrapperEl.append(row);
     [...row.children].forEach((column, colIndex) => {
       switch (colIndex) {
@@ -14,11 +20,35 @@ export default async function decorate(block) {
           column.className = 'card-title';
           if (column.children.length === 2) column.lastElementChild.className = 'title-unit';
           break;
-        default:
+        case 2:
           column.className = 'card-description';
+          break;
+        default:
+          column.className = 'card-step';
           break;
       }
     });
   });
+
   block.append(cardNavigationWrapperEl);
+
+  function moveCardStepBeforeTitle() {
+  // 获取所有需要调整的卡片（如果有多个）
+    const cards = block.querySelectorAll('.zh-card-box');
+
+    cards.forEach((card) => {
+      const stepElement = card.querySelector('.card-step');
+      const titleElement = card.querySelector('.card-title');
+
+      // 确保两个元素都存在且在中文网站页面
+      if (stepElement && titleElement && language === 'zh') {
+        // 将 step 移动到 title 前面
+        card.insertBefore(stepElement, titleElement);
+      } else {
+        // 步骤属性只在中文页面中展示
+        stepElement.remove();
+      }
+    });
+  }
+  moveCardStepBeforeTitle();
 }
