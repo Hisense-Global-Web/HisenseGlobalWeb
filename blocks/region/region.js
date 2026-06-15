58fda0fc69569247384869bdd6590bc19ae50936
肖航 <hang.xiao@accenture.cn>
2026-06-15 15:19:31 +0800

    region

--- blocks/region/region.js
+++ blocks/region/region.js
@@ -1,3 +1,24 @@
 export default async function decorate(block) {
-  console.log(block);
+  const regionList = document.createElement('div');
+  regionList.className = 'region-list';
+  [...block.children].forEach((row, index) => {
+    if (!index) {
+      row.className = 'region-title';
+    } else {
+      row.className = 'region-country';
+      regionList.appendChild(row);
+      const countryTitle = row.children[0].textContent.trim();
+      row.children[0].style.display = 'none';
+      const countryLink = row.children[1].querySelector('a').href;
+      row.children[1].style.display = 'none';
+      const countryEl = document.createElement('div');
+      countryEl.className = 'country-item';
+      countryEl.textContent = countryTitle;
+      countryEl.addEventListener('click', () => {
+        window.location.href = countryLink;
+      });
+      row.appendChild(countryEl);
+    }
+  });
+  block.appendChild(regionList);
 }
