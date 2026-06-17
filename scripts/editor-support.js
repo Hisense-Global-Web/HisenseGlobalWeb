a52720a3455624c0b41eabb99e023fc7cc919bf9
肖航 <hang.xiao@accenture.cn>
2026-06-17 11:44:49 +0800

    test

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -131,3 +131,5 @@ decorateRichtext();
 // for new richtext-instrumented elements. this happens for example when using experimentation.
 const observer = new MutationObserver(() => decorateRichtext());
 observer.observe(document, { attributeFilter: ['data-richtext-prop'], subtree: true });
+
+document.querySelector('aside');
