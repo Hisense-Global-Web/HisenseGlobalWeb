d73705fabfcc3c92274413761393cbec036fd10d
Lv <shanshan.lv@accenture.cn>
2026-06-26 16:28:36 +0800

    feat: enterprise tech add dynamic logic

--- blocks/enterprise-tech/enterprise-tech.js
+++ blocks/enterprise-tech/enterprise-tech.js
@@ -7,66 +7,73 @@ export default async function decorate(block) {
   const enterpriseCardWrapperEl = document.createElement('div');
   enterpriseCardWrapperEl.className = 'enterprise-card-wrapper';
 
-  let isDynamicFlag = ''; // dynamic media 标识
   [...block.children].forEach((row) => {
     row.className = 'enterprise-card-box info-hide';
     const infoBoxEl = document.createElement('div');
     infoBoxEl.className = 'info-box';
     const mobileTitWrapperEl = document.createElement('div');
     mobileTitWrapperEl.className = 'mobile-tit-wrapper';
-    [...row.children].forEach((column, colIndex) => {
-      switch (colIndex) {
-        case 0:
-          isDynamicFlag = column.textContent.trim() === 'true';
-          // column.remove();
-          break;
-        case 1:
-          column.className = 'pc-image-box';
-          if (column.querySelector('a') && isDynamicFlag) {
-            const dynamicImgSrc = column.querySelector('a').getAttribute('href');
-            column.append(createDynamicMediaPicture(dynamicImgSrc, 'enterprise-card-item-img'));
-            column.children[0].remove();
-          }
-          break;
-        case 2:
-          column.className = 'mobile-image-box';
-          break;
-        case 3:
-          column.className = 'title-box';
-          break;
-        case 4:
-          column.className = 'subtitle-box';
-          infoBoxEl.append(column);
-          break;
-        default:
-          column.className = 'description-box';
-          infoBoxEl.append(column);
-      }
-      if (colIndex === 2) {
-        const mobileArrowEl = document.createElement('div');
-        mobileArrowEl.className = 'mobile-arrow';
-        const arrowImg = document.createElement('img');
-        arrowImg.src = `/content/dam/hisense/${country}/common-icons/chevron-white-up.svg`;
-        arrowImg.alt = 'Arrow image';
-        mobileArrowEl.append(arrowImg);
+    const [dynamicSwitch, pcImgDom, mobileImgDom, titleBox, subtitleBox, descriptionBox] = [...row.children] ?? [];
+    const isDynamicFlag = dynamicSwitch.textContent.trim() === 'true'; // dynamic media 标识
+    dynamicSwitch.remove();
 
-        const mobileTitBoxEl = document.createElement('div');
-        mobileTitBoxEl.className = 'mobile-tit-box';
-        // title-box element
-        const titleBoxEl = row.children[2];
-        mobileTitBoxEl.append(titleBoxEl, mobileArrowEl);
-        mobileTitWrapperEl.append(column, mobileTitBoxEl);
-        // mobile arrow add click event
-        const toggleExpand = (e) => {
-          e.stopPropagation();
-          const grandParent = e.target.closest('.enterprise-card-box');
-          if (!grandParent) { return; }
-          grandParent.classList.toggle('info-hide');
-        };
-
-        mobileArrowEl.addEventListener('click', toggleExpand);
+    if (pcImgDom) {
+      pcImgDom.className = 'pc-image-box';
+      if (pcImgDom.querySelector('a') && isDynamicFlag) {
+        const dynamicImgSrc = pcImgDom.querySelector('a').getAttribute('href');
+        pcImgDom.append(createDynamicMediaPicture(dynamicImgSrc, 'enterprise-card-item-img'));
+        pcImgDom.children[0].remove();
       }
-    });
+    }
+
+    if (mobileImgDom) {
+      // mobile 端最终展示的 dom
+      let showMobileDom = mobileImgDom;
+      showMobileDom.className = 'mobile-image-box';
+      if (isDynamicFlag) {
+        // 如果启用dynamic media, 直接clone PC 端 img dom 元素，用来动态展示mobile 的图片
+        showMobileDom = pcImgDom.cloneNode(true);
+        showMobileDom.className = 'mobile-image-box';
+        mobileImgDom.remove();
+      }
+      // 移动端图片处理
+      const mobileArrowEl = document.createElement('div');
+      mobileArrowEl.className = 'mobile-arrow';
+      const arrowImg = document.createElement('img');
+      arrowImg.src = `/content/dam/hisense/${country}/common-icons/chevron-white-up.svg`;
+      arrowImg.alt = 'Arrow image';
+      mobileArrowEl.append(arrowImg);
+
+      const mobileTitBoxEl = document.createElement('div');
+      mobileTitBoxEl.className = 'mobile-tit-box';
+      // 移动端，将标题和箭头元素整合到一个dom 中
+      mobileTitBoxEl.append(titleBox, mobileArrowEl);
+      // 将移动端图片与 标题dom 整合
+      mobileTitWrapperEl.append(showMobileDom, mobileTitBoxEl);
+      // mobile arrow add click event
+      const toggleExpand = (e) => {
+        e.stopPropagation();
+        const grandParent = e.target.closest('.enterprise-card-box');
+        if (!grandParent) { return; }
+        grandParent.classList.toggle('info-hide');
+      };
+
+      mobileArrowEl.addEventListener('click', toggleExpand);
+    }
+
+    if (titleBox) {
+      titleBox.className = 'title-box';
+    }
+
+    if (subtitleBox) {
+      subtitleBox.className = 'subtitle-box';
+      infoBoxEl.append(subtitleBox);
+    }
+
+    if (descriptionBox) {
+      descriptionBox.className = 'description-box';
+      infoBoxEl.append(descriptionBox);
+    }
     row.append(mobileTitWrapperEl, infoBoxEl);
     enterpriseCardWrapperEl.append(row);
   });
