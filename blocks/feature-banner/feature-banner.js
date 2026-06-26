1fd1bf0a0b3c47092d7282ee6ada63f3b544b9de
Lv <shanshan.lv@accenture.cn>
2026-06-26 17:36:39 +0800

    feat: feature banner add dynamic media

--- blocks/feature-banner/feature-banner.js
+++ blocks/feature-banner/feature-banner.js
@@ -1,4 +1,28 @@
+// import { createDynamicMediaPicture } from '../hero-banner/media-reference.js';
 export default function decorate(block) {
+  // const [dynamicSwitch, featurePcImg, featureMobileImg] = [...block.children] ?? [];
+  // const isDynamicFlag = dynamicSwitch.textContent.trim() === 'true';
+  // dynamicSwitch.remove();
+
+  // if (featurePcImg) {
+  //   featurePcImg.className = 'feature-pc-img';
+  //   if (featurePcImg.querySelector('a') && isDynamicFlag) {
+  //     // 容错判断（兼容之前没有设置dynamic media 的组件）
+  //     const dynamicImgSrc = featurePcImg.querySelector('a').getAttribute('href');
+  //     featurePcImg.append(createDynamicMediaPicture(dynamicImgSrc, 'feature-banner'));
+  //     featurePcImg.children[0].remove();
+  //   }
+  // }
+
+  // if (featureMobileImg) {
+  //   featureMobileImg.className = 'feature-mobile-img';
+  //   if (isDynamicFlag) {
+  //     const cloneFeaturePcImg = featurePcImg.cloneNode(true);
+  //     cloneFeaturePcImg.className = 'feature-mobile-img';
+  //     block.append(cloneFeaturePcImg);
+  //     featureMobileImg.remove();
+  //   }
+  // }
   [...block.children].forEach((row, idx) => {
     if (idx === 0) {
       row.className = 'feature-pc-img';
