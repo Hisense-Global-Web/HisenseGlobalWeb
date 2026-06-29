846b6473302c0c1571bfcd046badb55d13995925
Lv <shanshan.lv@accenture.cn>
2026-06-29 17:19:09 +0800

    feat: reset card grid js

--- blocks/card-grid/card-grid.js
+++ blocks/card-grid/card-grid.js
@@ -8,52 +8,52 @@ export default async function decorate(block) {
     child.className = 'feature-item-box';
     const itemTextBoxEl = document.createElement('div');
     itemTextBoxEl.className = 'item-text-box';
-    // [...child.children].forEach((item, itemIndex) => {
-    //   switch (itemIndex) {
-    //     case 0:
-    //       item.className = 'item-img-box';
-    //       break;
-    //     case 1:
-    //       item.classList.add('item-subtitle');
-    //       itemTextBoxEl.append(item);
-    //       break;
-    //     case 2:
-    //       item.classList.add('item-title');
-    //       itemTextBoxEl.append(item);
-    //       break;
-    //     default:
-    //       item.classList.add('item-text');
-    //       itemTextBoxEl.append(item);
-    //   }
-    // });
-
-    const [dynamicSwitch, imgDom, subtitleDom, titDom, textDom] = [...block.children] ?? [];
-    const isDynamicFlag = dynamicSwitch.textContent.trim() === 'true';
-    dynamicSwitch.remove();
-
-    if (imgDom) {
-      imgDom.classList.add('item-img-box');
-      if (isDynamicFlag && imgDom.querySelector('a')) {
-        const dynamicImgSrc = imgDom.querySelector('a').getAttribute('href');
-        imgDom.append(createDynamicMediaPicture(dynamicImgSrc, 'card-grid-img'));
-        imgDom.children[0].remove();
+    [...child.children].forEach((item, itemIndex) => {
+      switch (itemIndex) {
+        case 0:
+          item.className = 'item-img-box';
+          break;
+        case 1:
+          item.classList.add('item-subtitle');
+          itemTextBoxEl.append(item);
+          break;
+        case 2:
+          item.classList.add('item-title');
+          itemTextBoxEl.append(item);
+          break;
+        default:
+          item.classList.add('item-text');
+          itemTextBoxEl.append(item);
       }
-    }
+    });
 
-    if (subtitleDom) {
-      subtitleDom.classList.add('item-subtitle');
-      itemTextBoxEl.append(subtitleDom);
-    }
+    // const [dynamicSwitch, imgDom, subtitleDom, titDom, textDom] = [...block.children] ?? [];
+    // const isDynamicFlag = dynamicSwitch.textContent.trim() === 'true';
+    // dynamicSwitch.remove();
 
-    if (titDom) {
-      titDom.classList.add('item-title');
-      itemTextBoxEl.append(titDom);
-    }
+    // if (imgDom) {
+    //   imgDom.classList.add('item-img-box');
+    //   if (isDynamicFlag && imgDom.querySelector('a')) {
+    //     const dynamicImgSrc = imgDom.querySelector('a').getAttribute('href');
+    //     imgDom.append(createDynamicMediaPicture(dynamicImgSrc, 'card-grid-img'));
+    //     imgDom.children[0].remove();
+    //   }
+    // }
 
-    if (textDom) {
-      textDom.classList.add('item-text');
-      itemTextBoxEl.append(textDom);
-    }
+    // if (subtitleDom) {
+    //   subtitleDom.classList.add('item-subtitle');
+    //   itemTextBoxEl.append(subtitleDom);
+    // }
+
+    // if (titDom) {
+    //   titDom.classList.add('item-title');
+    //   itemTextBoxEl.append(titDom);
+    // }
+
+    // if (textDom) {
+    //   textDom.classList.add('item-text');
+    //   itemTextBoxEl.append(textDom);
+    // }
 
     child.append(itemTextBoxEl);
     featureItemsWrapperEl.append(child);
