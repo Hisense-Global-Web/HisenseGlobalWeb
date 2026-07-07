24f7bdc813db0efdac53649f4883de2831ec5a20
guoil-zhang <guoli.zhang@accenture.cn>
2026-07-07 18:09:20 +0800

    Zhanggl media (#1018)
    * feat: update campaign collection, campaign image, download.

--- blocks/campaign-image/campaign-image.js
+++ blocks/campaign-image/campaign-image.js
@@ -1,31 +1,28 @@
-import { getSwitchValue } from '../../utils/ue-helper.js';
-import { createDynamicMediaPicture } from '../hero-banner/media-reference.js';
+import { checkSwitch, getSwitchValue } from '../../utils/ue-helper.js';
+import { checkDyanmicMediaImage } from '../hero-banner/media-reference.js';
 
 export default function decorate(block) {
+  const hasDynamicMedia = checkSwitch(block.children[0]);
+  if (!hasDynamicMedia) {
+    block.prepend(document.createElement('div'));
+  }
   const [enableDynamicMediaEl, pcImageEl, mobileImageEl] = [...block.children] ?? [];
   const isEnableDynamicMedia = getSwitchValue(enableDynamicMediaEl);
   enableDynamicMediaEl?.remove();
+  const dynamicPCImageEl = checkDyanmicMediaImage(pcImageEl);
   if (isEnableDynamicMedia) {
-    const dynamicImageUrl = pcImageEl.querySelector('a')?.href ?? null;
-    if (dynamicImageUrl) {
-      const dynamicImageEl = createDynamicMediaPicture(dynamicImageUrl, 'Campaign Image');
-      dynamicImageEl.className = 'pc-image';
-      block.append(dynamicImageEl);
-      const cloneDynamicImageEl = dynamicImageEl.cloneNode(true);
-      cloneDynamicImageEl.className = 'mobile-image';
-      block.append(cloneDynamicImageEl);
-    } else {
-      /* eslint-disable-next-line no-console */
-      console.warn('Dynamic image url is not found. Please check the image url.');
-    }
-    pcImageEl?.remove();
+    const cloneDynamicImageEl = dynamicPCImageEl.cloneNode(true);
+    dynamicPCImageEl.className = 'pc-image';
+    block.append(dynamicPCImageEl);
+    cloneDynamicImageEl.className = 'mobile-image';
+    block.append(cloneDynamicImageEl);
     mobileImageEl?.remove();
   } else {
+    if (dynamicPCImageEl) {
+      dynamicPCImageEl.className = 'pc-image';
+    }
     if (mobileImageEl) {
       mobileImageEl.className = 'mobile-image';
-    }
-    if (pcImageEl) {
-      pcImageEl.className = 'pc-image';
     }
   }
 }
