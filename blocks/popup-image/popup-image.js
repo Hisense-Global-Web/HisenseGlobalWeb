cec80de210739abf606a45c3673175a50c0f1dae
lvss-33 <shanshan.lv@accenture.cn>
2026-07-08 10:37:54 +0800

    feat: core-value-card, sustainability-dashboard-achievement block add dynamic media; popup-image fault tolerance (#1020)

--- blocks/popup-image/popup-image.js
+++ blocks/popup-image/popup-image.js
@@ -8,7 +8,7 @@ export default function decorate(block) {
   const [dynamicSwitch, popupImgDom] = [...block.children] ?? [];
   // const isDynamicFlag = dynamicSwitch.textContent.trim() === 'true';
   dynamicSwitch.remove();
-  if (popupImgDom.querySelector('a')) {
+  if (popupImgDom && popupImgDom.querySelector('a')) {
     // 设置dynamic media
     const dynamicImgSrc = popupImgDom.querySelector('a').getAttribute('href');
     popupImgDom.append(createDynamicMediaPicture(dynamicImgSrc, 'popup-image'));
