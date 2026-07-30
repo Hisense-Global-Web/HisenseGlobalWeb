a25e2ce16a27fdfa1a64dd00599a41634a8c5d58
Lv <shanshan.lv@accenture.cn>
2026-07-30 16:14:50 +0800

    feat: boli popup info card add default image

--- blocks/popup-info-card/popup-info-card.js
+++ blocks/popup-info-card/popup-info-card.js
@@ -1,16 +1,31 @@
+import { getLocaleFromPath } from '../../scripts/locale-utils.js';
+
 export default function decorate(block) {
-  console.log('block', block);
+  const { country } = getLocaleFromPath();
   [...block.children].forEach((item) => {
     item.classList = 'info-card-item';
     const columns = [...item.children];
-    columns.forEach((col) => {
-      if (col.querySelector('img')) {
-        col.classList = 'card-image';
-      } else {
-        col.classList = 'card-description';
-        const [title, text] = [...col.children];
-        title.classList = 'card-tit';
-        text.classList = 'card-text';
+    columns.forEach((col, index) => {
+      switch (index) {
+        case 0: {
+          col.classList = 'card-image';
+          if (!col.querySelector('img')) {
+            const defaultImg = document.createElement('img');
+            defaultImg.src = `/content/dam/hisense/${country}/common-icons/default-info-card.png`;
+            defaultImg.alt = 'default-info-card-img';
+            col.append(defaultImg);
+          }
+          break;
+        }
+        case 1: {
+          col.classList = 'card-description';
+          const [title, text] = [...col.children];
+          title.classList = 'card-tit';
+          text.classList = 'card-text';
+          break;
+        }
+        default:
+          break;
       }
     });
   });
